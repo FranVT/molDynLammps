0e7b10350a79b63bf6dd6384ad04ace7af298e02
Francisco Javier Vazquez Tavares <vazqueztfj@proton.me>
2026-01-21 15:11:30 -0600

    Bitacoras y simualciones prueba

--- polymericGels/gel-bonds-1/analysis/functions.jl
+++ polymericGels/gel-bonds-1/analysis/functions.jl
@@ -2,6 +2,142 @@
     Functions 
 """
 
+function fig_EngSys(cm, pt, dt,DATA)
+
+fig=Figure(size = (15cm, 12cm), fontsize = 12pt);
+
+clbr=:managua10;
+
+ax=Axis(fig[1:1,1:1],
+    title=latexstring("\\mathrm{Energy~System}"),
+    #subtitle=latexstring(subtitle),
+    xlabel=L"\mathrm{Time~units}~[\tau^*]",
+    ylabel=L"\langle U \rangle~[J/\epsilon]",
+    titlesize=24.0f0,
+    subtitlesize=20.0f0,
+    xticklabelsize=18.0f0,
+    yticklabelsize=18.0f0,
+    xlabelsize=22.0f0,
+    ylabelsize=22.0f0,
+    xminorticksvisible=true,
+    xminorgridvisible=true,
+    limits=(nothing,nothing,nothing,nothing), 
+    #yticks = 0:0.01:1.5*T
+    #xticks=domain
+   )
+
+plot!(ax,dt.*DATA.TimeStep,DATA."v_eT",label=L"\mathrm{eT}")
+plot!(ax,dt.*DATA.TimeStep,DATA."v_ec",label=L"\mathrm{ecouple}")
+plot!(ax,dt.*DATA.TimeStep,DATA."v_eC",label=L"\mathrm{eConserve}")
+
+#hlines!(ax,[T])
+
+Legend(fig[1,2],ax,
+      L"\mathrm{Legend}",
+     labelsize=12pt)
+
+    return fig
+
+end
+
+function fig_EngB(cm, pt, dt,DATA)
+
+fig=Figure(size = (15cm, 12cm), fontsize = 12pt);
+
+clbr=:managua10;
+
+ax=Axis(fig[1:1,1:1],
+    title=latexstring("\\mathrm{Energy~Bonds}"),
+    xlabel=L"\mathrm{Time~units}~[\tau^*]",
+    ylabel=L"\langle U \rangle~[J/\epsilon]",
+    titlesize=24.0f0,
+    subtitlesize=20.0f0,
+    xticklabelsize=18.0f0,
+    yticklabelsize=18.0f0,
+    xlabelsize=22.0f0,
+    ylabelsize=22.0f0,
+    xminorticksvisible=true,
+    xminorgridvisible=true,
+    limits=(nothing,nothing,nothing,nothing), 
+   )
+
+plot!(ax,dt.*DATA.TimeStep,DATA."v_eB",label=L"\mathrm{eB}")
+plot!(ax,dt.*DATA.TimeStep,DATA."v_eA",label=L"\mathrm{eA}")
+plot!(ax,dt.*DATA.TimeStep,DATA."v_eM",label=L"\mathrm{eM}")
+
+
+Legend(fig[1,2],ax,
+      L"\mathrm{Legend}",
+     labelsize=12pt)
+
+    return fig
+
+end
+
+
+
+function fig_Eng(cm, pt, dt,DATA)
+
+fig=Figure(size = (15cm, 12cm), fontsize = 12pt);
+
+clbr=:managua10;
+
+ax=Axis(fig[1:1,1:1],
+    title=latexstring("\\mathrm{Energy}"),
+    xlabel=L"\mathrm{Time~units}~[\tau^*]",
+    ylabel=L"\langle U \rangle~[J/\epsilon]",
+    titlesize=24.0f0,
+    subtitlesize=20.0f0,
+    xticklabelsize=18.0f0,
+    yticklabelsize=18.0f0,
+    xlabelsize=22.0f0,
+    ylabelsize=22.0f0,
+    xminorticksvisible=true,
+    xminorgridvisible=true,
+    limits=(nothing,nothing,nothing,nothing), 
+   )
+
+plot!(ax,dt.*DATA.TimeStep,DATA."c_ep",label=L"\mathrm{ep}")
+plot!(ax,dt.*DATA.TimeStep,DATA."c_ek",label=L"\mathrm{ek}")
+
+
+Legend(fig[1,2],ax,
+      L"\mathrm{Legend}",
+     labelsize=12pt)
+
+    return fig
+
+end
+
+function fig_Temp(cm, pt, dt,DATA,T)
+
+fig=Figure(size = (15cm, 12cm), fontsize = 12pt);
+
+clbr=:managua10;
+
+ax=Axis(fig[1:1,1:1],
+    title=latexstring("\\mathrm{Temperature}"),
+    xlabel=L"\mathrm{Time~units}~[\tau^*]",
+    ylabel=L"\langle T \rangle~[kBT/\epsilon]",
+    titlesize=24.0f0,
+    subtitlesize=20.0f0,
+    xticklabelsize=18.0f0,
+    yticklabelsize=18.0f0,
+    xlabelsize=22.0f0,
+    ylabelsize=22.0f0,
+    xminorticksvisible=true,
+    xminorgridvisible=true,
+    limits=(0,nothing,0,nothing), 
+    yticks = 0:0.01:1.5*T
+   )
+
+plot!(ax,dt.*DATA.TimeStep,DATA."c_t")
+hlines!(ax,[T])
+
+    return fig
+
+end
+
 function extractFixScalar(path_system,file_name)
 """
     Function that extracts the information of fix files that stores global scalar values
