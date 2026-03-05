685eaac3118cdd7a6e1dc3ff31d26d943e6ec285
Francisco Javier Vazquez Tavares <vazqueztfj@proton.me>
2026-03-05 12:34:06 -0600

    Reporte del potencial de 3 cuerpos. Simulaciones de prueba

--- polymericGels/tests-3body/src/sim/runSim-move.sh
+++ polymericGels/tests-3body/src/sim/runSim-move.sh
@@ -31,4 +31,4 @@ mkdir "$dir_save";
 mkdir "$dir_save/traj";
 files_name=("system_assembly.fixf" "stress_assembly.fixf" "clust_assembly.fixf" "profiles_assembly.fixf" "traj_assembly.*.dumpf" "data.hydrogel" "system_shear.fixf" "stress_shear.fixf" "clust_shear.fixf" "profiles_shear.fixf" "traj_shear.*.dumpf" "strain-*.restart")
 
-mpirun -np 1 lmp -sf omp -in in.tests-move3-mix.lmp -log $log_name -var L $L -var tstep $dt -var Nsave $Nsave -var NsaveStress $NsaveStress -var Ndump $Ndump -var Dir $dir_save -var file1_name ${files_name[0]} -var file2_name ${files_name[1]} -var file3_name ${files_name[2]} -var file4_name ${files_name[3]} -var file5_name ${files_name[4]} -var file6_name ${files_name[5]};
+mpirun -np 1 lmp -sf omp -in in.move3-2types.lmp -log $log_name -var L $L -var tstep $dt -var Nsave $Nsave -var NsaveStress $NsaveStress -var Ndump $Ndump -var Dir $dir_save -var file1_name ${files_name[0]} -var file2_name ${files_name[1]} -var file3_name ${files_name[2]} -var file4_name ${files_name[3]} -var file5_name ${files_name[4]} -var file6_name ${files_name[5]};
