143127d6eb0a4d03aa801e85d393ef7cc642ab63
Francisco Javier Vazquez Tavares <vazqueztfj@proton.me>
2026-03-18 14:32:21 -0600

    Preparar scripts para set de simulaciones

---
+++ polymericGels/gel-bonds-1/src/sim/createDatFile_assembly.sh
@@ -0,0 +1,80 @@
+: '
+    Bash script that creates the data file for the assembly simulation
+'
+
+#!/bin/bash
+
+# Input variables
+dir_system=$1
+dir_src=$2
+id=$3
+id=$4
+
+# Load the parameters file
+source load_parameters.sh system.parameters
+
+# Load the assembly config file
+source load_parameters.sh assembly$id-$var_ccL.parameters
+
+# Go to the directory in which the files are going to be created 
+cd $dir_system;
+
+# Create the data.dat file
+file_name="dataAssembly.dat";
+
+# Initialize arrays to hold headers and values
+headers=()
+values=()
+
+# Populate headers and values arrays
+for i in "${!files_name[@]}"; do
+    headers+=("file$i")
+    values+=("${files_name[$i]}")
+done
+
+headers+=("phi")
+values+=("${phi}")
+
+headers+=("CL-Con")
+values+=("${var_ccL}")
+
+headers+=("Npart")
+values+=("${N_particles}")
+
+headers+=("Temperature")
+values+=("${T}")
+
+headers+=("damp")
+values+=("${damp}")
+
+headers+=("time-step")
+values+=("${dt}")
+
+headers+=("N_heat")
+values+=("${steps_heat}")
+
+headers+=("N_isot")
+values+=("${steps_isot}")
+
+headers+=("N_CL")
+values+=("${N_CL}")
+
+headers+=("N_MO")
+values+=("${N_MO}")
+
+headers+=("L")
+values+=("${L}")
+
+headers+=("save-dump")
+values+=("${Ndump}")
+
+headers+=("save-fix")
+values+=("${Nsave}")
+
+headers+=("save-stress")
+values+=("${NsaveStress}")
+
+# Write headers and values to the file
+echo "$(IFS=,; echo "${headers[*]}")" > "$file_name"
+echo "$(IFS=,; echo "${values[*]}")" >> "$file_name"
+
--- polymericGels/gel-bonds-1/src/sim/runSim.sh
+++ polymericGels/gel-bonds-1/src/sim/runSim.sh
@@ -4,56 +4,54 @@
 
 #!/bin/bash
 
-# Volume of patchy particles
-Vol_MO1=0.5376050428;
-Vol_CL1=0.5516113101;
+# Main Directories
+dir_home="/home/franvt/GitRepos/molDynLammps/polymericGels/gel-bonds-1";
+dir_src="$dir_home/src";
+dir_sim="$dir_src/sim";
+dir_data="$dir_home/data";
+dir_save="$dir_data/$id";
 
-# Assign values to the independent variables
-T=0.05;
-N_particles=500;
-phi=0.01;
-CL_con=0.05;
 
-# Assign computational parameters
-damp=1;
-dt=0.001;
-seed1=3234;
-seed2=6321;
-seed3=2010;
+# Load the parameters file
+source load_parameters.sh system.parameters
 
-steps_isot=8500000;
-steps_heat=500000;
+# Create the id for the simulation
+id="${T}${phi}${CL_con}${N_particles}-$(date +%F-%H%M%S)";
 
-log_name="log-test.log";
 
-# Assign significant decimals
-cs=6;
+# Create the config fle
+bash createConfigFile_assembly.sh $dir_src $id
 
 # Compute values for dependent variables
-N_CL=$(echo "scale=0; $CL_con * $N_particles" | bc);
-N_CL=${N_CL%.*};
-N_MO=$(( $N_particles - $N_CL ));
-Vol_MO=$(echo "scale=$cs; $Vol_MO1 * $N_MO" | bc);         # Vol of N f=2 patchy particles
-Vol_CL=$(echo "scale=$cs; $Vol_CL1 * $N_CL" | bc);          # Vol of N f=4 patchy particles 
-Vol_Totg=$(echo "scale=$cs; $Vol_MO + $Vol_CL" | bc);       # Total volume of a mixture of N f=2 and M f=4 patchy particles
-Vol_Tot=$(echo "scale=$cs; $Vol_Totg / $phi" | bc);
-L_real=$(echo "scale=$cs; e( (1/3) * l($Vol_Tot) )" | bc -l );
-L=$(echo "scale=$cs; $L_real / 2" | bc);
-aux2=$(echo "scale=$cs; 1 / $damp" | bc);
-aux=$(echo "scale=$cs; 1 / $dt" | bc);
-Nsave=$(echo "scale=0; $aux2 * $aux" | bc);
-Nsave=${Nsave%.*};
-NsaveStress=$(echo "scale=0; $aux2 * $aux" | bc);
-NsaveStress=${NsaveStress%.*};
-Ndump=$(echo "scale=0; $aux" | bc);
-Ndump=${Ndump%.*};
+#N_CL=$(echo "scale=0; $CL_con * $N_particles" | bc);
+#N_CL=${N_CL%.*};
+#N_MO=$(( $N_particles - $N_CL ));
+#Vol_MO=$(echo "scale=$cs; $Vol_MO1 * $N_MO" | bc);         # Vol of N f=2 patchy particles
+#Vol_CL=$(echo "scale=$cs; $Vol_CL1 * $N_CL" | bc);          # Vol of N f=4 patchy particles 
+#Vol_Totg=$(echo "scale=$cs; $Vol_MO + $Vol_CL" | bc);       # Total volume of a mixture of N f=2 and M f=4 patchy particles
+#Vol_Tot=$(echo "scale=$cs; $Vol_Totg / $phi" | bc);
+#L_real=$(echo "scale=$cs; e( (1/3) * l($Vol_Tot) )" | bc -l );
+#L=$(echo "scale=$cs; $L_real / 2" | bc);
+#aux2=$(echo "scale=$cs; 1 / $damp" | bc);
+#aux=$(echo "scale=$cs; 1 / $dt" | bc);
+#Nsave=$(echo "scale=0; $aux2 * $aux" | bc);
+#Nsave=${Nsave%.*};
+#NsaveStress=$(echo "scale=0; $aux2 * $aux" | bc);
+#NsaveStress=${NsaveStress%.*};
+#Ndump=$(echo "scale=0; $aux" | bc);
+#Ndump=${Ndump%.*};
+
+# Create the data file for the assembly simulation
+bash  createDatFile_assembly.sh $dir_system $dir_src $id
+
 
 # Assign values to file management variables
-dir_data="/home/franvt/GitRepos/molDynLammps/polymericGels/gel-bonds-1/data";
-id="${T}${phi}${CL_con}${N_particles}-$(date +%F-%H%M%S)";
-dir_save="$dir_data/$id";
-mkdir "$dir_save";
-mkdir "$dir_save/traj";
+
+#mkdir "$dir_save";
+#mkdir "$dir_save/traj";
 files_name=("system_assembly.fixf" "stress_assembly.fixf" "clust_assembly.fixf" "profiles_assembly.fixf" "traj_assembly.*.dumpf" "data.hydrogel" "system_shear.fixf" "stress_shear.fixf" "clust_shear.fixf" "profiles_shear.fixf" "traj_shear.*.dumpf" "strain-*.restart")
 
-mpirun -np 8 lmp -sf omp -in in.assembly.lmp -log $log_name -var temp $T -var damp $damp -var L $L -var NCL $N_CL -var NMO $N_MO -var seed1 $seed1 -var seed2 $seed2 -var seed3 $seed3 -var tstep $dt -var Nsave $Nsave -var NsaveStress $NsaveStress -var Ndump $Ndump -var steps $steps_isot -var stepsheat $steps_heat -var Dir $dir_save -var file1_name ${files_name[0]} -var file2_name ${files_name[1]} -var file3_name ${files_name[2]} -var file4_name ${files_name[3]} -var file5_name ${files_name[4]} -var file6_name ${files_name[5]};
+# Create the data file for the assembly simulation
+bash create-file_dataAssembly.sh $dir_save $dir_src $var_ccL $id
+
+#mpirun -np 8 lmp -sf omp -in in.assembly.lmp -log $log_name -var temp $T -var damp $damp -var L $L -var NCL $N_CL -var NMO $N_MO -var seed1 $seed1 -var seed2 $seed2 -var seed3 $seed3 -var tstep $dt -var Nsave $Nsave -var NsaveStress $NsaveStress -var Ndump $Ndump -var steps $steps_isot -var stepsheat $steps_heat -var Dir $dir_save -var file1_name ${files_name[0]} -var file2_name ${files_name[1]} -var file3_name ${files_name[2]} -var file4_name ${files_name[3]} -var file5_name ${files_name[4]} -var file6_name ${files_name[5]};
