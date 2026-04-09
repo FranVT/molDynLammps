bc32398c1e9abbf75ce403d6d099852a4c5aff69
Francisco Javier Vazquez Tavares <vazqueztfj@proton.me>
2026-04-09 12:48:03 -0600

    Graficas

--- polymericGels/gel-batch-1/analysis/graphs.jl
+++ polymericGels/gel-batch-1/analysis/graphs.jl
@@ -168,6 +168,106 @@ function patchEnergyFig(dat_DF,system_DF)
 
     save(joinpath(pwd(),"patchyEnergy.png"), fig, px_per_unit = 300/inch)
 end
+
+function numberClustersFig(dat_DF,system_DF)
+"""
+    Function that stores a comparisson of potential energy
+"""
+    fig=Figure()
+    ax_f=Axis(fig[1:1,1:1])
+    ax_f2=Axis(fig[1:1,1:1])
+
+    ax=Axis(fig[1:3,1:1],
+        title=latexstring("\\mathrm{Number~of~clusters}"),
+        #subtitle=latexstring(subtitle),
+        xlabel=L"\mathrm{Time~units}~[\tau^*]",
+        ylabel=L"N",
+        xminorticksvisible=true,
+        xminorgridvisible=true,
+        limits=(0,nothing,0,nothing),
+        #xscale=log10,
+        #yscale=log10
+    )
+
+    hidespines!(ax_f)
+    hidedecorations!(ax_f)
+    hidespines!(ax_f2)
+    hidedecorations!(ax_f2)
+
+    linkyaxes!(ax,ax_f,ax_f2)
+    linkxaxes!(ax,ax_f,ax_f2)
+
+    for it in 1:nrow(dat_DF)
+        plot!(ax,system_DF[it].Nclustres,
+            label=latexstring(100*dat_DF.phi[it]),
+            )
+        plot!(ax_f,[-1],[0],
+            label=latexstring(100*dat_DF."CL-Con"[it])
+            )
+        plot!(ax_f2,[-1],[0],
+            label=latexstring(dat_DF."Temperature"[it])
+            )
+    end
+
+    Legend(fig[1,2],ax,L"\phi~\%")
+    Legend(fig[2,2],ax_f,L"\mathrm{CL}~\%")
+    Legend(fig[3,2],ax_f2,L"\mathrm{T}")
+
+    save(joinpath(pwd(),"NClusters.png"), fig, px_per_unit = 300/inch)
+end
+
+function maxPartClustersFig(dat_DF,system_DF)
+"""
+    Function that stores a comparisson of potential energy
+"""
+    fig=Figure()
+    ax_f=Axis(fig[1:1,1:1])
+    ax_f2=Axis(fig[1:1,1:1])
+
+    ax=Axis(fig[1:3,1:1],
+        title=latexstring("\\mathrm{Particles~in~biggest~cluster}"),
+        #subtitle=latexstring(subtitle),
+        xlabel=L"\mathrm{Time~units}~[\tau^*]",
+        ylabel=L"N",
+        xminorticksvisible=true,
+        xminorgridvisible=true,
+        limits=(0,nothing,0,nothing),
+        #xscale=log10,
+        #yscale=log10
+    )
+
+    hidespines!(ax_f)
+    hidedecorations!(ax_f)
+    hidespines!(ax_f2)
+    hidedecorations!(ax_f2)
+
+    linkyaxes!(ax,ax_f,ax_f2)
+    linkxaxes!(ax,ax_f,ax_f2)
+
+    for it in 1:nrow(dat_DF)
+        plot!(ax,system_DF[it].Maxparticles,
+            label=latexstring(100*dat_DF.phi[it]),
+            )
+        plot!(ax_f,[-1],[0],
+            label=latexstring(100*dat_DF."CL-Con"[it])
+            )
+        plot!(ax_f2,[-1],[0],
+            label=latexstring(dat_DF."Temperature"[it])
+            )
+    end
+
+    Legend(fig[1,2],ax,L"\phi~\%")
+    Legend(fig[2,2],ax_f,L"\mathrm{CL}~\%")
+    Legend(fig[3,2],ax_f2,L"\mathrm{T}")
+
+    save(joinpath(pwd(),"MaxPartClusters.png"), fig, px_per_unit = 300/inch)
+end
+
+
+
+
+
+
 #=
     Script
 =#
@@ -183,10 +283,22 @@ dat_DF=getDatInfo(DF_DIR);
 # Get the info of each system
 system_DF=getInfoSystem(INFO_DIR,dat_DF.file_system);
 
+
+
+
+
+cluster_DF=getInfoSystem(INFO_DIR,string.("clusterObservables_",dat_DF.id_ref,".csv"))
+
+
+
+
+
+
 #=
     Figures
 =#
 
-potentialEnergyFig(dat_DF,system_DF)
-patchEnergyFig(dat_DF,system_DF)
-
+#potentialEnergyFig(dat_DF,system_DF)
+#patchEnergyFig(dat_DF,system_DF)
+#numberClustersFig(dat_DF,cluster_DF)
+maxPartClustersFig(dat_DF,cluster_DF)
