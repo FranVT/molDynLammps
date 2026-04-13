929185f5b853a3c15f29e93eed2598576b88a64d
Francisco Javier Vazquez Tavares <vazqueztfj@proton.me>
2026-04-13 12:52:38 -0600

    Simulaciones/Empezar calculo de factor de estructura

--- polymericGels/gel-batch-1/analysis/sandbox.jl
+++ polymericGels/gel-batch-1/analysis/sandbox.jl
@@ -79,6 +79,65 @@ TRAJ_DIR=joinpath(DATA_DIR,dat_DF.dir[DIR_id],"traj");
 
 aux_dump=getDump(TRAJ_DIR,string("traj_assembly.",M_frames,".dumpf"));
 
+# Extraer las posiciones del dump de las partículas centrales.
+mask=(aux_dump.type .==1) .| (aux_dump.type .== 2.0);
+df_filtered=aux_dump[mask,:];
+
+rx=df_filtered.x;
+ry=df_filtered.y;
+rz=df_filtered.z;
+r=[rx ry rz];
+
+# número de partículas en la simulación
+N_part=dat_DF.Npart[DIR_id];
+
 # Compute Structure factor
+
+# Vector de onda
+#=
+    Se obtienen vectores de onda agrupados por su  magnitud
+=#
+
+tol=10;
+grupos=Dict{Float64, Vector{Tuple{Float64,Float64,Float64}}}();
+
+N=30;
+Q_max=sqrt(3*(2*pi/2*dat_DF.L[5])^2);
+
+for nx in -N:N, ny in -N:N, nz in -N:N
+    qx=2*pi*nx/2*dat_DF.L[5];
+    qy=2*pi*ny/2*dat_DF.L[5];
+    qz=2*pi*nz/2*dat_DF.L[5];
+    qq=qx^2 + qy^2 + qz^2;
+    q=sqrt(qq);
+    
+    if q<Q_max || q == 0.0
+        continue
+    end
+
+    clave=round(qq, digits=tol);
+    push!(get!(grupos,clave,[]),(qx,qy,qz))
+end
+
+# Compute the structure factor per magnitude
+Sq_observable=[];
+
+for (clave, lista_q) in grupos
+    q_mod=sqrt(clave);
+    S_acumulado=0.0;
+    for v_q in lista_q
+        qr=v_q[1].*rx .+ v_q[2].*ry .+ v_q[3].*rz;
+        S_q=(1/N_part)*(sum(cos.(qr))^2 + sum(sin.(qr))^2);
+        S_acumulado += S_q;
+    end
+    S_promedio = S_acumulado / length(lista_q);
+    push!(Sq_observable,(q_mod,S_promedio));
+end
+
+# Ordena de mayor a menor
+Sq_observable=sort!(Sq_observable);
+
+
+
 
 
