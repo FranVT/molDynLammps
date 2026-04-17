538e89c665cd9d7e9c8968d16e687ad5b1a87220
Francisco Javier Vazquez Tavares <vazqueztfj@proton.me>
2026-04-17 11:04:31 -0600

    Otras simulaciones

---
+++ polymericGels/gel-batch-2/README.md
@@ -0,0 +1,10 @@
+# Directory for batch simulations
+
+Batch de simulaciones con las siguientes configuraciones:
+\phi\in\{0.01, 0.05 \}
+CL\% = 5\% = 0.05
+Npart = 5000
+T = 0.05
+dt=0.01
+damp = 1.0
+
---
+++ polymericGels/gel-batch-2/src/sim/runSim.sh
@@ -0,0 +1,44 @@
+: '
+    Script that runs the assembly procotol with given parameters.
+'
+
+#!/bin/bash
+
+# Main Directories
+dir_home="/home/franvt/GitRepos/molDynLammps/polymericGels/gel-batch-1";
+dir_src="$dir_home/src";
+dir_sim="$dir_src/sim";
+dir_data="$dir_home/data";
+
+
+# Load the parameters file
+source load_parameters.sh system.parameters
+
+for var_phi in $(seq $phi_o $phi_d $phi_f);
+do
+    for var_Nexp in $(seq 1 1 $NexpT);
+    do
+        # Create the id for the simulation
+        id="$(date +%F-%H%M%S)_${var_Nexp}";
+
+        # Directory to save the information Directory to save the informationn
+        dir_save="$dir_data/$id";
+
+        # Create the config fle
+        bash createConfigFile_assembly.sh $dir_sim $id $var_phi $var_Nexp
+
+        # Create directory to save data
+        mkdir "$dir_save";
+        mkdir "$dir_save/traj";
+
+        # Create the data file for the assembly simulation
+        bash  createDatFile_assembly.sh $dir_save $dir_src $id
+
+        # Load the config file
+        source load_parameters.sh assembly$id.config 
+
+        mpirun -np 8 lmp -sf omp -in in.assembly.lmp -var temp $T -var damp $damp -var L $L -var NCL $N_CL -var NMO $N_MO -var seed1 $seed1 -var seed2 $seed2 -var seed3 $seed3 -var tstep $dt -var Nsave $Nsave -var NsaveStress $NsaveStress -var Ndump $Ndump -var steps $steps_isot -var stepsheat $steps_heat -var Dir $dir_save -var file1_name ${files_name[0]} -var file2_name ${files_name[1]} -var file3_name ${files_name[2]} -var file4_name ${files_name[3]};
+
+        mv log.lammps "$dir_save"
+    done
+done
