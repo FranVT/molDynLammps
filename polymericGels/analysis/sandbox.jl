72059e95e65654d7e1a9ab178d441074c70d8e04
Francisco Javier Vazquez Tavares <vazqueztfj@proton.me>
2026-04-23 14:27:38 -0600

    Factor de estructura

--- polymericGels/analysis/sandbox.jl
+++ polymericGels/analysis/sandbox.jl
@@ -18,7 +18,7 @@ SAVE_DIR=joinpath(MAIN_DIR,"analyzedData");
 dat_files=CSV.read(DAT_PATH,DataFrame);
 
 # Selection of the system by parameters
-phi=0.02;
+phi=0.05;
 Temp=0.05;
 N_part=5000.0;
 CL_con=0.05;
@@ -35,14 +35,15 @@ dat_DF = subset(dat_files,
 dump_paths=joinpath.(dat_DF.PARENT_DIR,dat_DF.dir,"traj");
 
 # Parametros para obtener el factor de estructura
-N_qu=2^2; # Cantidad de direcciones
+N_qu=2^5; # Cantidad de direcciones
 lambda_o=1; # Limites del rango a explorar (Monomero)
 lambda_f=2*dat_DF.L[1]; # Limites del rango a explorar (Tamaño de la caja)
-N_lambda=2^3; # Cantidad de magnitudes
+N_lambda=2^7; # Cantidad de magnitudes
 
 # Seleccion de time instants
-aux_id=Int.((0:dat_DF."save-dump"[1]:(dat_DF."N_heat"[1] + dat_DF."N_isot"[1])));
-aux_id=aux_id[1:100:end];
+aux_timeStep=Int.((0:dat_DF."save-dump"[1]:(dat_DF."N_heat"[1] + dat_DF."N_isot"[1])));
+ind=round.(Int, LinRange(1, length(aux_timeStep), 10));
+aux_id=aux_timeStep[ind];
 
 time_instants=[replace("traj_assembly.*.dumpf", "*" => string(it)) for it in aux_id];
 
@@ -56,8 +57,5 @@ Sq_df=DataFrame(;
     lambda_f = fill(lambda_f, length(aux_id)),
     id=unique(dat_DF.id)[1]
 )
-
-# CSV.write(joinpath(SAVE_DIR,"structureFactor.csv"),Sq_df)
-
 
 CSV.write(joinpath(SAVE_DIR,string("structureFactor",Sq_df.id[1],".csv")),Sq_df)
