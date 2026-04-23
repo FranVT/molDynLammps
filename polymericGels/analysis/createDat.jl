abb184e8b60dcabb2ed10d47bb355cfe4546f2dc
Francisco Javier Vazquez Tavares <vazqueztfj@proton.me>
2026-04-23 10:22:49 -0600

    Modificaciones para le analisis

--- polymericGels/analysis/createDat.jl
+++ polymericGels/analysis/createDat.jl
@@ -11,11 +11,9 @@ using UUIDs, LinearAlgebra
 include("functions.jl")
 
 # Get all directories in the data directory
-MAIN_DIR=dirname(pwd());
-DATA_DIR="/run/media/franvt/rogelio/DinMol/gel-batch-1";
+MAIN_DIR=pwd();
+DATA_DIR="/run/media/franvt/rogelio/DinMol/gel-batch-1/data/";
 STORE_DIR=joinpath(MAIN_DIR,"datFiles");
-
-
 
 # Read the directory where data is stored
 SIMS_DIR=filter(isdir,readdir(DATA_DIR,join=true));
@@ -23,11 +21,9 @@ SIMS_DIR=filter(isdir,readdir(DATA_DIR,join=true));
 # Get all data files in a dataframe
 data_info=mapreduce(s->getDat(s),vcat,SIMS_DIR);
 
-# Ids
-id=string(uuid4());
-file_name_dat=string("dat-$phi-$Temp-$N_part-$CL_con.csv");
-file_name_info=string("system_info_$id.csv");
+# Add related paths
+data_info[!,:PARENT_DIR].=[DATA_DIR];
 
-# Save the data
-CSV.write(joinpath(pwd(),"data_mod",file_name_info),data_system);
-CSV.write(joinpath(pwd(),"data_mod",file_name_dat),data_filtrada);
+# Store the data
+CSV.write(string(STORE_DIR,"/experiments_dat.csv"),data_info);
+
