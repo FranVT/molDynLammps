323e0a284d222841661c1e009256b8fc4ebcd37c
Francisco Javier Vazquez Tavares <vazqueztfj@proton.me>
2026-05-05 16:11:42 -0600

    Inicio factor de estructura

--- polymericGels/analysis/structureFactor_v2.jl
+++ polymericGels/analysis/structureFactor_v2.jl
@@ -13,7 +13,7 @@ Random.seed!(1234)
 # Include auxiliary files
 include("functions.jl")
 
-    function computeDensity(q_x,q_y,q_z,dist_x,dist_y,dist_z)
+    function computeDensity(q_x,q_y,q_z,dist_x,dist_y,dist_z,N_qu)
         
         # Calcular el producto punto. 
         # Misma magnitud. Distintas direcciones
@@ -23,7 +23,7 @@ include("functions.jl")
         A=map(id->mapreduce(s->cos(s),+,l[id,:]),1:N_qu);
         B=map(id->mapreduce(s->sin(s),+,l[id,:]),1:N_qu);
         rho_q=A.^2 .+ B.^2;
-        return rho
+        return rho_q
     end
 
     function computeDistances(r,lambda_f)
@@ -76,6 +76,9 @@ lambda_f=2*dat_DF.L[1]; # Limites del rango a explorar (Tamaño de la caja)
 N_lambda=64; # Cantidad de magnitudes
 N_instants=2;
 
+
+#=
+
 # Seleccion de time instants
 aux_timeStep=Int.((0:dat_DF."save-dump"[1]:(dat_DF."N_heat"[1] + dat_DF."N_isot"[1])));
 ind=round.(Int, LinRange(1, length(aux_timeStep), N_instants));
@@ -94,6 +97,11 @@ time_instant=time_instants[end];
     theta=2*pi*rand(N_theta);
     phi=pi*rand(N_phi); 
 
+    # Calcular la densidad promedio de cada magnitud
+    q_min=2*pi/lambda_f;
+    q_max=2*pi/lambda_o;
+    q_dom=range(q_min,q_max,length=N_lambda);
+
     # Para todas las distancias
     ids_utri=reduce(vcat,[[[r,c] for c in r+1:N_part] for r in 1:N_part]);
 
@@ -102,11 +110,6 @@ time_instant=time_instants[end];
     q_x=mapreduce(ph->map(th->cos(th)*sin(ph),theta),vcat,phi); 
     q_y=mapreduce(ph->map(th->sin(th)*sin(ph),theta),vcat,phi);
     q_z=mapreduce(ph->map(th->cos(ph),theta),vcat,phi); 
-
-    # Calcular la densidad promedio de cada magnitud
-    q_min=2*pi/lambda_f;
-    q_max=2*pi/lambda_o;
-    q_dom=range(q_min,q_max,length=N_lambda);
 
     # Evaluar distintas magnitudes
     q_x=[mag*q_x for mag in q_dom];
@@ -127,7 +130,7 @@ time_instant=time_instants[end];
 
         for it_q in eachindex(q_dom)
             # Compute the density
-            rho_q=computeDensity(q_x[it_q],q_y[it_q],q_z[it_q],dist_x,dist_y,dist_z);
+            rho_q=computeDensity(q_x[it_q],q_y[it_q],q_z[it_q],dist_x,dist_y,dist_z,N_qu);
 
             # Obtener el factor de structura para una magnitud
             S_q[it_r][it_q]=mean(rho_q)/N_part;
@@ -137,6 +140,51 @@ time_instant=time_instants[end];
         println(it_r)
 
     end
+
+=#
+
+fig=Figure()
+ax=Axis(fig[1:1,1:1],
+        limits=(nothing,nothing,nothing,nothing),
+
+       )
+scatterlines!(ax,q_dom,mean(S_q))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
