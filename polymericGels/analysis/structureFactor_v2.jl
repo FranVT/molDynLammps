853c833556ae14c165b2146bf2d17e2aaf7acfeb
Francisco Javier Vazquez Tavares <vazqueztfj@proton.me>
2026-05-08 12:16:28 -0600

    Fractor de estructura y correcciones

--- polymericGels/analysis/structureFactor_v2.jl
+++ polymericGels/analysis/structureFactor_v2.jl
@@ -6,8 +6,7 @@
 using DataFrames, CSV
 using Statistics, StatsBase
 using UUIDs, LinearAlgebra
-using Distances, Random
-using GLMakie, JLD
+using Random
 
 Random.seed!(1234)
 
@@ -22,7 +21,7 @@ SAVE_DIR=joinpath(MAIN_DIR,"analyzedData");
 dat_files=CSV.read(DAT_PATH,DataFrame);
 
 # Selection of the system by parameters
-phi=0.01;
+phi=0.02;
 Temp=0.05;
 N_part=5000.0;
 CL_con=0.05;
@@ -39,9 +38,10 @@ dat_DF = subset(dat_files,
 dump_paths=joinpath.(dat_DF.PARENT_DIR,dat_DF.dir,"traj");
 
 # Parametros para obtener el factor de estructura
-N_qu=2^9; # EXPONENTE DEBE SER IMPAR Cantidad de direcciones
 lambda_o=1; # Limites del rango a explorar (Monomero)
 lambda_f=2*dat_DF.L[1]; # Limites del rango a explorar (Tamaño de la caja)
+N_part=Int(dat_DF.Npart[1]);
+N_qu=2^9; # EXPONENTE DEBE SER IMPAR Cantidad de direcciones
 N_lambda=2^8; # Cantidad de magnitudes
 N_instants=2;
 
@@ -49,33 +49,22 @@ N_instants=2;
 aux_timeStep=Int.((0:dat_DF."save-dump"[1]:(dat_DF."N_heat"[1] + dat_DF."N_isot"[1])));
 ind=round.(Int, LinRange(1, length(aux_timeStep), N_instants));
 aux_id=aux_timeStep[ind];
-
 time_instants=[replace("traj_assembly.*.dumpf", "*" => string(it)) for it in aux_id];
 
-time_instant=time_instants[end];
+# Get the structure factor for different time intervals
+S_q=[zeros(N_lambda) for _ in eachindex(time_instants)];
+for it_t in eachindex(time_instants)
+    S_q[it_t]=computeStructureFactor(N_qu,N_lambda,lambda_o,lambda_f,time_instants[it_t],dump_paths);
+end
 
-N_part=Int(dat_DF.Npart[1]);
+Sq_df=DataFrame(;
+    timeStep = aux_id,
+    q_domain = fill(range(2*pi/lambda_f,2*pi/lambda_o,length=N_lambda), length(aux_id)),
+    Sq       = S_q,
+    lambda_o = fill(lambda_o, length(aux_id)),
+    lambda_f = fill(lambda_f, length(aux_id)),
+    id=unique(dat_DF.id)[1]
+)
 
-S_q=computeStructureFactor(N_qu,N_lambda,lambda_o,lambda_f,time_instant,dump_paths);
-
-q_min=2*pi/lambda_f;
-q_max=2*pi/lambda_o;
-q_dom=range(q_min,q_max,length=N_lambda);
-
-
-fig=Figure()
-ax=Axis(fig[1:1,1:1],
-        limits=(nothing,nothing,0,10)
-       )
-#vlines!(ax,2*pi/(1.2),linestyle=:dash,color=:blue)
-#vlines!(ax,2*pi/(2*1.2),linestyle=:dash,color=:blue)
-#vlines!(ax,2*pi/(3*1.2),linestyle=:dash,color=:blue)
-
-#vlines!(ax,2*pi/(lambda_f),linestyle=:solid,color=:black)
-#vlines!(ax,2*pi/(0.5*lambda_f),linestyle=:solid,color=:black)
-#vlines!(ax,2*pi/(0.25*lambda_f),linestyle=:solid,color=:black)
-#vlines!(ax,2*pi/(0.125*lambda_f),linestyle=:solid,color=:black)
-#vlines!(ax,2*pi/(0.0625*lambda_f),linestyle=:solid,color=:black)
-
-scatterlines!(ax,(2pi)./q_dom,S_q)
+CSV.write(joinpath(SAVE_DIR,string("structureFactor",Sq_df.id[1],".csv")),Sq_df)
 
