8ebd5ebc8d7d2a8753d832e726d2c90713100f97
Francisco Javier Vazquez Tavares <vazqueztfj@proton.me>
2026-05-27 17:39:56 -0600

    Resultados factor de estructura

--- polymericGels/analysis/sandbox.jl
+++ polymericGels/analysis/sandbox.jl
@@ -38,6 +38,7 @@ function structureFactor(pos_exp, mag_n, n, qo, N_part; N_exp=length(pos_exp))
             rho[:, it_exp] = computeDensity(PP)
         end
 
+        # Promedio de direcciones y experimentos
         # Sq = mean(rho, dims=2) ./ N_part;  mean(Sq) == mean(rho) / N_part
         sq[i] = mean(rho) / N_part
     end
@@ -53,7 +54,7 @@ function dumpAnalysis(dat_DF)
     # Parameters of the system relevant to the structure factor
     L=2*dat_DF.L[1];    # Longitud de la caja
     N_instants=2;       # Instantes temporales a analizar
-    n_max=2^5;          # Magnitud máxima de cada componente
+    n_max=2^2;          # Magnitud máxima de cada componente
     N_exp=nrow(dat_DF); # Cantidad de experimentos por sistema
     N_part=dat_DF.Npart[1];
     qo=2*pi/L;
@@ -100,6 +101,10 @@ function dumpAnalysis(dat_DF)
 end
 
 
+#function avgSq(dat_DF)
+#    return mean([dumpAnalysis(dat_DF) for s in dat_DF])
+#end
+
 
 # Extraer la información del dat file
 dat_files=extractDatFiles();
@@ -110,16 +115,11 @@ categories=[:phi];
 # Creación de los subdataframes por sistema
 data_bySystem=groupby(dat_files,categories);
 
-#dump=zeros(length(data_bySystem));
-
-# Argumentos de la función para analizar el factor de estructura
-#id_system=5;
-
-# Dat file of the system to be analyzed
-#dat_DF=data_bySystem[id_system];
-
 # Compute the analysis from dump files for all the systems
-#Sq_exp=[dumpAnalysis(dat_DF) for dat_DF in data_bySystem];
+Sq_all=[dumpAnalysis(dat_DF) for dat_DF in data_bySystem];
+
+# Pasar los resultados a un DataFrame
+df=map(s->DataFrame(s,[:nMag, :Sqo, :Sqf]),Sq_all);
 
 
 nothing
