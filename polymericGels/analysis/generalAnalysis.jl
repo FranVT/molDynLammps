8f8c5dbce37406ba4c66265fa8b6830d3f33fb8c
Francisco Javier Vazquez Tavares <vazqueztfj@proton.me>
2026-05-29 13:33:49 -0600

    Fractor de estructura y correcciones

--- polymericGels/analysis/generalAnalysis.jl
+++ polymericGels/analysis/generalAnalysis.jl
@@ -17,7 +17,7 @@ include("functions_graphs.jl")
 # Activate functions
 up=0;
 Sq=0;
-Sq_PBC=1;
+Sq_PBC=0;
 
 
 # Update the data file with new systems
@@ -39,7 +39,7 @@ if Sq_PBC == 1
     storeAllSq(data_bySystem,[dumpAnalysis(dat_DF) for dat_DF in data_bySystem])
 end
 
-#=
+
 #figureCompareSq(dat_files)
 #figureCompareSl(dat_files)
 
@@ -60,10 +60,9 @@ dict_T=Dict(dat_files.id.=>dat_files.Temperature);
 
 # Get the information
 Sq=[CSV.read(file,DataFrame) for file in file_paths];
-=#
 
+ids=unique(dat_files.id);
 
-#=
 println("Se inicia a graficar")
 fig=Figure()
     ax_f=Axis(fig[1:1,1:1])
@@ -73,11 +72,11 @@ fig=Figure()
     ax=Axis(fig[1:6,1:3],
         title=latexstring("\\mathrm{Structure~factor}"),
         #subtitle=latexstring(subtitle),
-        xlabel=L"|\vec{q}|",
+        xlabel=L"|\vec{q}|=\frac{2\pi}{L}|\vec{n}|",
         ylabel=L"S(q)",
         xminorticksvisible=true,
         xminorgridvisible=true,
-        limits=(0,2,0,25),
+        limits=(0,nothing,0,nothing),
         #xscale=log10,
         #yscale=log10
     )
@@ -91,6 +90,17 @@ fig=Figure()
     #vlines!(ax,1.2,linestyle=:dash,color=:grey)
     #vlines!(ax,5*1.2,linestyle=:dash,color=:grey)
 
+    for it_sys in 1:5
+        q=(2*pi).*sqrt.(Sq[it_sys].mag)./first(data_bySystem[it_sys].L)
+        lambda=2*pi./q
+        scatterlines!(ax,q,Sq[it_sys].Sq10000000,
+               label=latexstring(100*first(data_bySystem[it_sys].phi))
+              )
+    end
+
+    Legend(fig[2,4],ax,L"\phi~\%",merge=true)
+
+    #=
     selec=1;
 
     for it in 1:N_instants
@@ -107,9 +117,9 @@ fig=Figure()
             label=latexstring(100*dict_phi[unique(Sq[selec].id)[1]])
             )
 
-    p1.visible = false
-    p2.visible = false
-    p3.visible = false
+        p1.visible = false
+        p2.visible = false
+        p3.visible = false
 
 
     end
@@ -118,7 +128,7 @@ fig=Figure()
     Legend(fig[3,4],ax_f,L"\mathrm{CL}~\%",merge=true)
     Legend(fig[4,4],ax_f2,L"\mathrm{T}",merge=true)
     Legend(fig[5,4],ax_f3,L"\phi",merge=true)
-
-save(string("fig_SqcompPBC_phi_",100*dict_phi[unique(Sq[selec].id)[1]],"_zoom.png"), fig, px_per_unit = 300/inch)
-
 =#
+
+save(string("fig_SqcompPBC_phi_q.png"), fig, px_per_unit = 300/inch)
+
