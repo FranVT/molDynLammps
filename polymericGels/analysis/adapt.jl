33ad7d39488bebe4de384116c5ddd06d0fdfab49
Francisco Javier Vazquez Tavares <vazqueztfj@proton.me>
2026-06-04 11:03:07 -0600

    Fractor de estructura

--- polymericGels/analysis/adapt.jl
+++ polymericGels/analysis/adapt.jl
@@ -156,6 +156,45 @@ function computeSqmean(file_names,timeStep,file_path,dump_paths,dat_DF,numbin,nt
     CSV.write(joinpath(file_path,filename),df)   
 end
 
+function computeAllTimeSqmean(dat_DF,N_instants,qmax0)
+"""
+    Compute all structure factor of all system of N time instants
+"""
+
+    # Parameters of the system relevant to the structure factor
+    L=2*dat_DF.L[1];            # Longitud de la caja
+    N_exp=nrow(dat_DF);         # Cantidad de experimentos por sistema
+    N_part=dat_DF.Npart[1];     # Numero de particulas centrales
+
+    # Crea los dominios del vector de onda
+    xc = L;                 # longitud de la caja en x
+    yc = L;                 # longitud de la caja en y
+    zc = L;                 # longitud de la caja en z
+    dq0 = 2*pi / xc;        # Δq fundamental
+    qmax = Int(floor(qmax0 / dq0)) # número entero de pasos hasta qmax0
+    rq0 = qmax * dq0;       # valor máximo real de |q| usado
+    bin0 = dq0;             # nuevo ancho de bin (bin0 original * dq0)
+    numbin = Int(floor(qmax * dq0 / bin0)) + 1;  # número total de bines
+
+    # Parametros para el factor de estructura
+    ntotav=Int(N_part); # Número total de partículas
+    (qxhis,qyhis,qzhis,qhis)=createqdom(qmax,rq0,dq0,bin0,numbin);  # vector de onda
+    file_path=joinpath(pwd(),"analyzedData");
+
+    # Obtener los paths y los nombres de los archivos a analizar
+    (timeSteps,dump_paths,file_names)=getpathfilesSq(dat_DF,N_instants);
+
+    # Select one time step
+    # Alocar memoria
+    info=[zeros(numbin,3) for _ in 1:N_exp];
+
+    for it_time in eachindex(file_names)
+        computeSqmean(file_names[it_time],timeSteps[it_time],file_path,dump_paths,dat_DF,numbin,ntotav,qxhis,qyhis,qzhis,qhis,info)
+        println("One time step done")
+    end
+    
+    println("One system done")
+end
 
 # Extraer la información del dat file
 dat_files=extractDatFiles();
@@ -171,281 +210,11 @@ data_bySystem=groupby(dat_files,categories);
 """
 
 # Select one system
-dat_DF=data_bySystem[1];
-
-# Parameters of the system relevant to the structure factor
-L=2*dat_DF.L[1];            # Longitud de la caja
+N_systems=length(data_bySystem);
 N_instants=2;               # Instantes temporales a analizar
-N_exp=nrow(dat_DF);         # Cantidad de experimentos por sistema
-N_part=dat_DF.Npart[1];     # Numero de particulas centrales
+qmax0 = 6;              # 3 es el min sin que cause problemas
 
-# Crea los dominios del vector de onda
-qmax0 = 3;              # 3 es el min sin que cause problemas
-xc = L;                 # longitud de la caja en x
-yc = L;                 # longitud de la caja en y
-zc = L;                 # longitud de la caja en z
-dq0 = 2*pi / xc;        # Δq fundamental
-qmax = Int(floor(qmax0 / dq0)) # número entero de pasos hasta qmax0
-rq0 = qmax * dq0;       # valor máximo real de |q| usado
-bin0 = dq0;             # nuevo ancho de bin (bin0 original * dq0)
-numbin = Int(floor(qmax * dq0 / bin0)) + 1;  # número total de bines
-
-# Parametros para el factor de estructura
-ntotav=Int(N_part); # Número total de partículas
-(qxhis,qyhis,qzhis,qhis)=createqdom(qmax,rq0,dq0,bin0,numbin);  # vector de onda
-file_path=joinpath(pwd(),"analyzedData");
-
-# Obtener los paths y los nombres de los archivos a analizar
-(timeSteps,dump_paths,file_names)=getpathfilesSq(dat_DF,N_instants);
-
-# Select one time step
-# Alocar memoria
-info=[zeros(numbin,3) for _ in 1:N_exp];
-
-for it_time in eachindex(file_names)
-    computeSqmean(file_names[it_time],timeSteps[it_time],file_path,dump_paths,dat_DF,numbin,ntotav,qxhis,qyhis,qzhis,qhis,info)
-    println("One time step done")
+for it_system in 1:N_systems
+    computeAllTimeSqmean(data_bySystem[it_system],N_instants,qmax0)
 end
-
-
-
-
-#map(s->CSV.write(joinpath(file_path,files_name[s]),Sq_all[s]),1:length(data_bySystem))
-
-#=
-# Aviso si alguna partícula está exactamente en el origen
-for i in 1:ntot
-    if r[i,1] == 0.0 && r[i,2] == 0.0 && r[i,3] == 0.0
-        println("No data")
-    end
-end
-
-## --- Cálculo del factor de estructura ---
-sfhis .= 0.0 # Se inicializa la variable en la que se guardara el factor de estructura
-for iq in -qmax:qmax
-    qr1 = iq * dq0          # componente x del vector q
-    for jq in -qmax:qmax
-        qr2 = jq * dq0      # componente y
-        for kq in -qmax:qmax
-            
-            # Excluir el origen del espacio recíproco
-            if iq == 0 && jq == 0 && kq == 0
-                continue
-            end
-            
-            # Componente z del vector q
-            qr3 = kq * dq0                     # componente z
-           
-            qr = [qr1, qr2, qr3]               # vector q
-            rq = sqrt(qr1^2 + qr2^2 + qr3^2)   # magnitud |q|
-            if rq > rq0
-                continue
-            end
-
-            # Determinar el índice del bin (manejo especial del borde)
-            sbin = floor(Int, rq / bin0) + 1
-
-            # Coso para trnasformar de un dominio continup a uno discreto 
-            if rq % bin0 == 0.0
-                sbin -= 1
-            end
-           
-            # Si la magnitud del vector supera la magnitud de interés se lo salta
-            if sbin > numbin
-                continue
-            end
-
-            # Producto escalar q·r para todas las partículas
-            kr = r[:, 1:3] * qr     # vector de tamaño ntot
-
-            # Sumas con pesos (partes real e imaginaria de la suma de exp(i q·r))
-            sfsum1 = sum(cos.(kr / s))  # parte real se divide entre el diametro de la partícula
-            sfsum2 = sum(sin.(kr / s))  # parte imaginaria
-
-            # Acumular |S(q)|^2 / N y aumentar el conteo del bin
-            sfhis[sbin, 1] += (sfsum1^2 + sfsum2^2) / ntotav
-            sfhis[sbin, 2] += 1
-        end
-    end
-end
-=#
-
-
-
-
-
-
-
-
-#=
-
-
-
-# Promedio del factor de estructura para cada magnitud
-sfhis[:, 1] = sfhis[:, 1] ./ sfhis[:, 2]
-
-# Encontrar el índice del bin con el valor máximo de S(q)
-kmax = argmax(sfhis[:, 1])
-
-# Valor máximo del factor de estructura
-smax = sfhis[kmax, 1]
-
-# Almacenar la información del factor de estructura
-info=zeros(numbin-1,3)
-
-# Bucle sobre todos los bines excepto el último (Fortran: ij=1,numbin-1)
-for ij in 1:numbin-1
-    ki = ij * bin0                        # centro del bin ij
-    sq = sfhis[ij, 1]                     # S(q) promedio
-    sq_norm = sq / smax                   # S(q) / S_max
-    info[ij,1]=ki
-    info[ij,2]=sq
-    info[ij,3]=sq_norm
-end
-
-
-# --- Cálculo del factor de estructura ---
-sfhis .= 0.0 # Se inicializa la variable en la que se guardara el factor de estructura
-for iq in -qmax:qmax
-    qr1 = iq * dq0          # componente x del vector q
-    for jq in -qmax:qmax
-        qr2 = jq * dq0      # componente y
-        for kq in -qmax:qmax
-            
-            # Excluir el origen del espacio recíproco
-            if iq == 0 && jq == 0 && kq == 0
-                continue
-            end
-            
-            # Componente z del vector q
-            qr3 = kq * dq0                     # componente z
-           
-            qr = [qr1, qr2, qr3]               # vector q
-            rq = sqrt(qr1^2 + qr2^2 + qr3^2)   # magnitud |q|
-            if rq > rq0
-                continue
-            end
-
-            # Determinar el índice del bin (manejo especial del borde)
-            sbin = floor(Int, rq / bin0) + 1
-
-            # Coso para trnasformar de un dominio continup a uno discreto 
-            if rq % bin0 == 0.0
-                sbin -= 1
-            end
-           
-            # Si la magnitud del vector supera la magnitud de interés se lo salta
-            if sbin > numbin
-                continue
-            end
-
-            # Producto escalar q·r para todas las partículas
-            kr = r[:, 1:3] * qr     # vector de tamaño ntot
-
-            # Sumas con pesos (partes real e imaginaria de la suma de exp(i q·r))
-            sfsum1 = sum(cos.(kr / s))  # parte real se divide entre el diametro de la partícula
-            sfsum2 = sum(sin.(kr / s))  # parte imaginaria
-
-            # Acumular |S(q)|^2 / N y aumentar el conteo del bin
-            sfhis[sbin, 1] += (sfsum1^2 + sfsum2^2) / ntotav
-            sfhis[sbin, 2] += 1
-        end
-    end
-end
- 
-# Promedio del factor de estructura para cada magnitud
-sfhis[:, 1] = sfhis[:, 1] ./ sfhis[:, 2]
-
-# Encontrar el índice del bin con el valor máximo de S(q)
-kmax = argmax(sfhis[:, 1])
-
-# Valor máximo del factor de estructura
-smax = sfhis[kmax, 1]
-
-# Almacenar la información del factor de estructura
-info=zeros(numbin-1,3)
-
-# Bucle sobre todos los bines excepto el último (Fortran: ij=1,numbin-1)
-for ij in 1:numbin-1
-    ki = ij * bin0                        # centro del bin ij
-    sq = sfhis[ij, 1]                     # S(q) promedio
-    sq_norm = sq / smax                   # S(q) / S_max
-    info[ij,1]=ki
-    info[ij,2]=sq
-    info[ij,3]=sq_norm
-end
-
-
- --- Cálculo del factor de estructura ---
-sfhis .= 0.0 # Se inicializa la variable en la que se guardara el factor de estructura
-for iq in -qmax:qmax
-    qr1 = iq * dq0          # componente x del vector q
-    for jq in -qmax:qmax
-        qr2 = jq * dq0      # componente y
-        for kq in -qmax:qmax
-            
-            # Excluir el origen del espacio recíproco
-            if iq == 0 && jq == 0 && kq == 0
-                continue
-            end
-            
-            # Componente z del vector q
-            qr3 = kq * dq0                     # componente z
-           
-            qr = [qr1, qr2, qr3]               # vector q
-            rq = sqrt(qr1^2 + qr2^2 + qr3^2)   # magnitud |q|
-            if rq > rq0
-                continue
-            end
-
-            # Determinar el índice del bin (manejo especial del borde)
-            sbin = floor(Int, rq / bin0) + 1
-
-            # Coso para trnasformar de un dominio continup a uno discreto 
-            if rq % bin0 == 0.0
-                sbin -= 1
-            end
-           
-            # Si la magnitud del vector supera la magnitud de interés se lo salta
-            if sbin > numbin
-                continue
-            end
-
-            # Producto escalar q·r para todas las partículas
-            kr = r[:, 1:3] * qr     # vector de tamaño ntot
-
-            # Sumas con pesos (partes real e imaginaria de la suma de exp(i q·r))
-            sfsum1 = sum(cos.(kr / s))  # parte real se divide entre el diametro de la partícula
-            sfsum2 = sum(sin.(kr / s))  # parte imaginaria
-
-            # Acumular |S(q)|^2 / N y aumentar el conteo del bin
-            sfhis[sbin, 1] += (sfsum1^2 + sfsum2^2) / ntotav
-            sfhis[sbin, 2] += 1
-        end
-    end
-end
- 
-# Promedio del factor de estructura para cada magnitud
-sfhis[:, 1] = sfhis[:, 1] ./ sfhis[:, 2]
-
-# Encontrar el índice del bin con el valor máximo de S(q)
-kmax = argmax(sfhis[:, 1])
-
-# Valor máximo del factor de estructura
-smax = sfhis[kmax, 1]
-
-# Almacenar la información del factor de estructura
-info=zeros(numbin-1,3)
-
-# Bucle sobre todos los bines excepto el último (Fortran: ij=1,numbin-1)
-for ij in 1:numbin-1
-    ki = ij * bin0                        # centro del bin ij
-    sq = sfhis[ij, 1]                     # S(q) promedio
-    sq_norm = sq / smax                   # S(q) / S_max
-    info[ij,1]=ki
-    info[ij,2]=sq
-    info[ij,3]=sq_norm
-end
-=#
-
 
