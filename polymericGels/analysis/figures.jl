3c2897bc9ceb49b7d8bd71ccbcbbdbb360af7ed4
Francisco Javier Vazquez Tavares <vazqueztfj@proton.me>
2026-06-17 10:06:32 -0600

    Mejorar metodologia de analisis de informacion

---
+++ polymericGels/analysis/figures.jl
@@ -0,0 +1,11 @@
+"""
+    Scripts to create figures
+"""
+
+using GLMakie, LaTeXStrings, Typst_jll
+
+# Grafica del factor de estructura
+MAIN_DIR=pwd();
+SAVE_DIR=joinpath(MAIN_DIR,"analyzedData");
+
+
