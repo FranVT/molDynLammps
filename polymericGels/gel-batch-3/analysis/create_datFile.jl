3ed18bf3be75b5b1612874d8cf96c6fd1d6daaa4
Francisco Javier Vazquez Tavares <vazqueztfj@proton.me>
2026-06-29 10:51:27 -0600

    Mejorar la documentacion

--- polymericGels/gel-batch-3/analysis/create_datFile.jl
+++ polymericGels/gel-batch-3/analysis/create_datFile.jl
@@ -12,6 +12,19 @@ using DataFrames, CSV
     parse_value(v::String)
 
 Function that converts a string into float, int or string automatically
+
+# Argument
+- `v::String`: String to be transformed.
+
+# Examples
+julia> parse_value("3.14")
+3.14
+
+julia> parse_value.(["3.14","2"])
+2-element Vector{Real}:
+ 3.14
+ 2
+
 """
 function parse_value(v::String)
     if (i = tryparse(Int, v)) !== nothing
@@ -24,8 +37,17 @@ function parse_value(v::String)
 end
 
 """
-    create_dfParams(path_file::String)
-Function that returns a dataframe of the params.log file given the path.
+    create_df_params(path_file::String)
+
+Read the parameter file `params.log` from the path `path_file` and return a DataFrame with a row containing all the parameters as columns. Additionally, add the columns `:id` (name of the containing folder) and `:dir` (parent directory).
+
+# Arguments
+- `path_file::String`: absolute path to the file `params.log`.
+
+# Example
+julia> df = create_df_params("/ruta/experimento/params.log")
+1×44 DataFrame
+
 """
 function create_dfParams(path_file::String)
     # Read the file
@@ -74,27 +96,25 @@ dir_main=pwd();
 dirs_info=filter(isdir, readdir(dir_data, join = true));
 
 # Name of the file
-FileLog="params.log";
+file_log="params.log";
 
 # Create the path to the data
-path_params=joinpath.(dir_data,dirs_info,FileLog);
+path_params=joinpath.(dir_data,dirs_info,file_log);
 
 # Read the params.log file
-df_params=[create_dfParams(s) for s in path_params];
+df_params=[create_df_params(s) for s in path_params];
 
 # Create a combine dataframe
 df_dat=reduce(vcat,df_params);
 
 # Define a file name for the data file
-FileDat="dat.csv";
+file_dat="dat.csv";
 
 # File path of dataframe data 
-file_path=joinpath(dir_main,FileDat);
+file_path=joinpath(dir_main,file_dat);
 
 # Save the dataframe of the metadata of the experiments 
 CSV.write(file_path,df_dat)
-
-
 
 # Search for files in each path
 #files_info=map(s->filter(isfile, readdir(s, join = true)),path_info);
