1a929c4f4ede436627f19b0248729f11d389f3d3
Francisco Javier Vazquez Tavares <vazqueztfj@proton.me>
2026-06-29 16:44:10 -0600

    Graficas de energia

---
+++ polymericGels/gel-batch-3/analysis/FixInfoFigures.jl
@@ -0,0 +1,152 @@
+#=
+    Script to create figures from the fix files info
+=#
+
+using DataFrames, CSV
+using GLMakie, LaTeXStrings 
+#=
+    Set figures 
+=#
+INCH = 96;
+PT = 4/3;
+CM = INCH / 2.54;
+
+set_theme!(
+    backgroundcolor = :white,
+    fontsize = 16PT,                     # tamaño de letra base
+    Axis = (
+        titlesize = 20PT,                 # tamaño del título del eje
+        xlabelsize = 14PT,                # tamaño etiqueta eje x
+        ylabelsize = 14PT,                 # tamaño etiqueta eje y
+        xticklabelsize = 12PT,             # tamaño números eje x
+        yticklabelsize = 12PT,              # tamaño números eje y
+        xgridstyle = :dash,               # estilo de la cuadrícula
+        ygridstyle = :dash,
+        spinewidth = 1.5PT,
+    ),
+    Legend = (
+        labelsize = 10PT,                   # tamaño texto leyenda
+        framewidth = 1.5PT,
+    ),
+    Colorbar = (
+        labelsize = 14PT,
+        ticklabelsize = 12PT,
+    ),
+    Figure = (
+        size = (15CM, 12CM)                 # tamaño de la figura (ancho, alto)
+    )
+)
+
+#=
+    Functions 
+=#
+
+
+
+
+#=
+    Start the script
+=#
+
+# Paths and directories
+DIR_MAIN = pwd();
+DIR_SAVE = joinpath(DIR_MAIN,"analyzed_data");
+FILE_DAT = "dat.csv";
+
+# Constants
+DT=0.001;
+
+# Read the directory 
+files=readdir(DIR_SAVE);
+
+# Read the files
+df_files=[CSV.read(joinpath(DIR_SAVE,file), DataFrame) for file in files];
+
+# Prepare time domains
+time_domains=(DT).*[df[!,:TimeStep] for df in df_files];
+
+
+
+
+
+
+# Prepare the labels
+    phis      = (100).*[first(unique(df[!,:phi])) for df in df_files];
+    phi_min   = minimum(phis);
+    phi_max   = maximum(phis);
+
+    # Prepare the color code
+    sys_domain  = eachindex(df_files);
+    N_systems   = length(df_files);
+    color_norm  = phis ./ N_systems;
+    color_min   = first(color_norm);
+    color_max   = last(color_norm);
+
+    fig = Figure()
+    ax_plot = Axis(fig[1:1, 1:1],
+                   xlabel = L"\mathrm{Time~}[\tau]",
+                   ylabel = L"K(\tau)~[\epsilon]",
+                   xminorticksvisible = true,
+                   xminorgridvisible = true,
+                   limits = (nothing, nothing, nothing, nothing),
+                   xscale = log10
+                  )
+
+    # Plot the potential energy for each system
+    for it_sys in sys_domain
+        scatterlines!(ax_plot, time_domains[it_sys], df_files[it_sys].c_ek,
+                      color = color_norm[it_sys],
+                      colorrange = (color_min, color_max)
+                     )
+    end
+
+    # Legends in terms of the packing fraction
+    Colorbar(fig[1, 2], label = L"\phi", colormap = :viridis, limits = (phi_min, phi_max))
+
+    save(string("fig_KineticEnergy_phiseries.png"), fig, px_per_unit = 300 / INCH)
+
+    fig = Figure()
+    ax_plot = Axis(fig[1:1, 1:1],
+                   xlabel = L"\mathrm{Time~}[\tau]",
+                   ylabel = L"U(\tau)~[\epsilon]",
+                   xminorticksvisible = true,
+                   xminorgridvisible = true,
+                   limits = (nothing, nothing, nothing, nothing),
+                   xscale = log10
+                  )
+
+    # Plot the potential energy for each system
+    for it_sys in sys_domain
+        scatterlines!(ax_plot, time_domains[it_sys], df_files[it_sys].c_ep,
+                      color = color_norm[it_sys],
+                      colorrange = (color_min, color_max)
+                     )
+    end
+
+    # Legends in terms of the packing fraction
+    Colorbar(fig[1, 2], label = L"\phi", colormap = :viridis, limits = (phi_min, phi_max))
+
+    save(string("fig_PotentialEnergy_phiseries.png"), fig, px_per_unit = 300 / INCH)
+
+    fig = Figure()
+    ax_plot = Axis(fig[1:1, 1:1],
+                   xlabel = L"\mathrm{Time~}[\tau]",
+                   ylabel = L"T(\tau)",
+                   xminorticksvisible = true,
+                   xminorgridvisible = true,
+                   limits = (nothing, nothing, nothing, nothing),
+                   xscale = log10
+                  )
+
+    # Plot the potential energy for each system
+    for it_sys in sys_domain
+        scatterlines!(ax_plot, time_domains[it_sys], df_files[it_sys].c_t,
+                      color = color_norm[it_sys],
+                      colorrange = (color_min, color_max)
+                     )
+    end
+
+    # Legends in terms of the packing fraction
+    Colorbar(fig[1, 2], label = L"\phi", colormap = :viridis, limits = (phi_min, phi_max))
+
+    save(string("fig_Temperature_phiseries.png"), fig, px_per_unit = 300 / INCH)
