112d55a3f05216a111c853f1bb612e14170c3527
Francisco Javier Vazquez Tavares <vazqueztfj@proton.me>
2026-06-30 11:56:45 -0600

    Factor de eestructura

--- polymericGels/gel-batch-3/analysis/StructureFactorAnalysis.jl
+++ polymericGels/gel-batch-3/analysis/StructureFactorAnalysis.jl
@@ -174,7 +174,7 @@ end
 
 Store a dataframe with the collective mean of S(q) of a set of simulations at a time step
 """
-function save_Sq_timestep_mean(paths_traj_timestep::Vector{String}, n_bin::Integer, n_exp::Integer, n_tot_av::Integer, qx_his::Vector{Vector{Any}}, qy_his::Vector{Vector{Any}}, qz_his::Vector{Vector{Any}}, q_his::Vector{Vector{Any}}, categories_system::Vector{Symbol}, categories_experiment::Vector{Symbol}, DIR_SAVE::String)
+function save_Sq_timestep_mean(df_set::AbstractDataFrame, paths_traj_timestep::Vector{String}, n_bin::Integer, n_exp::Integer, n_tot_av::Integer, qx_his::Vector{Vector{Any}}, qy_his::Vector{Vector{Any}}, qz_his::Vector{Vector{Any}}, q_his::Vector{Vector{Any}}, q_mean::Vector{Float64}, categories_system::Vector{Symbol}, categories_experiment::Vector{Symbol}, DIR_SAVE::String)
     println("Start one save\n")
 
     # Get the dump
@@ -279,7 +279,7 @@ function save_Sq_set(df_set::AbstractDataFrame,n_steps_Sq::Integer, categories_s
     (qx_his, qy_his, qz_his, q_his, q_mean) = createqdom(qmax, rq_0, dq_0, bin_0, n_bin);
 
     # Store the assembly mean of a structure factor for the time steps selected
-    foreach(s->save_Sq_timestep_mean(paths_dumpf_simulations[s,:],n_bin,n_exp,n_tot_av,qx_his,qy_his,qz_his,q_his,categories_system,categories_experiment,DIR_SAVE),1:n_steps_Sq)
+    foreach(s->save_Sq_timestep_mean(df_set,paths_dumpf_simulations[s,:],n_bin,n_exp,n_tot_av,qx_his,qy_his,qz_his,q_his,q_mean,categories_system,categories_experiment,DIR_SAVE),1:n_steps_Sq)
 
     println("End one time series")
 end
@@ -300,27 +300,25 @@ n_steps_Sq=2;
 # Define the maximum wave vector
 qmax_0=2;
 
-# Read the dat file
-df_dat=CSV.read(joinpath(DIR_MAIN,FILE_DAT), DataFrame);
-
 # Select the categories that define a system
 categories_system=[:phi,:chi_4,:temp,:damp];
 
 # Create categories to select different experiments (Just in case)
 categories_experiment=[:N_heat,:N_isothermal];
 
+# Read the dat file
+df_dat=CSV.read(joinpath(DIR_MAIN,FILE_DAT), DataFrame);
+
 # Group by system 
 df_systems=groupby(df_dat,categories_system);
 
-# Select one system
-df_aux=df_systems[1];
+# Save the mean of S(q) of a system given a set of experiments and a time domain
+for df_aux in df_systems
+    # Group by experiments
+    df_experiments=groupby(df_aux,categories_experiment);
 
-# Group by experiments
-df_experiments=groupby(df_aux,categories_experiment);
+    # Save the mean of S(q) of an experiment given a set of simulation and a time domain 
+    foreach(df_set->save_Sq_set(df_set,n_steps_Sq,categories_system,categories_experiment,DIR_SAVE) ,df_experiments)
+end
 
-# Select one set of experiments
-df_set=df_experiments[1];
-
-# Save the mean of S(q) of an experiment given a set of simulation and a time domain 
-foreach(df_set->save_Sq_set(df_set,n_steps_Sq,categories_system,categories_experiment,DIR_SAVE) ,df_experiments)
 
