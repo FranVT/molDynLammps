9116ed4873570288e7f218f2e234e60d3d4c2d40
Francisco Javier Vazquez Tavares <vazqueztfj@proton.me>
2026-07-01 10:41:44 -0600

    structure factor figures

--- polymericGels/gel-batch-3/analysis/StructureFactorFigures.jl
+++ polymericGels/gel-batch-3/analysis/StructureFactorFigures.jl
@@ -341,6 +341,227 @@ end
 
 end
 
+"""
+    fig_sq_phiseries_loglog(df_combo::DataFrame, categories_figures::Vector{Symbol})
+
+Creates a figure that compares the final configuration structure factor for different packing fractions
+"""
+function fig_sq_phiseries_loglog(df_combo::DataFrame, categories_figures::Vector{Symbol})
+# Get the biggest timestep
+time_max= minimum(df_combo.timeStep);
+
+# Get all rows at the max time
+df_aux=df_combo[df_combo.timeStep .== time_max, :];
+
+# Get the domain of the packing fraction
+phi_domain=unique(df_aux.phi);
+
+# Get the data for the labels
+labels_plot=[df_aux[1, col] for col in categories_figures] # Waring: This only works for one case
+
+    # Prepare some labels
+    phi_min=minimum(phi_domain);
+    phi_max=maximum(phi_domain);
+
+    # Prepare the color code
+    color_norm  = phi_domain ./ phi_max;
+    color_min   = first(color_norm);
+    color_max   = last(color_norm);
+
+# Group by system and experiments
+df_systems=groupby(df_aux,categories_figures);
+
+    # Starts the figure
+    fig = Figure()
+    ax_raw_plot = Axis(fig[1:3, 1:1],
+                   #xlabel = L"|\vec{q}|",
+                   ylabel = L"\langle S(|\vec{q}|)\rangle",
+                   xminorticksvisible = true,
+                   xminorgridvisible = true,
+                   limits = (10^(-1.5), 10^1, 10^(-1.5), nothing),
+                   xscale = log10,
+                   yscale = log10
+                  )
+
+    for df_plot in df_systems
+        # Get the domain
+        domain=df_plot.q_mean[:];
+    
+        # Get the range
+        range=df_plot.Sq_mean[:];
+
+        # Get the color
+        color_label=first(unique(df_plot.phi))/phi_max;
+
+        # Plot
+        scatterlines!(ax_raw_plot,domain,range,
+                      color = color_label,
+                      colorrange = (color_min, color_max))
+    end
+
+    ax_norm_plot = Axis(fig[4:6, 1:1],
+                   xlabel = L"|\vec{q}|",
+                   ylabel = L"\langle S(|\vec{q}|)\rangle/S_{\max}(|\vec{q}|)",
+                   xminorticksvisible = true,
+                   xminorgridvisible = true,
+                   limits = (10^(-1.5), 10^1, nothing, nothing),
+                   xscale = log10,
+                   yscale = log10
+                  )
+
+    for df_plot in df_systems
+        # Get the domain
+        domain=df_plot.q_mean[:];
+    
+        # Get the range
+        range=df_plot.Sq_mean_norm[:];
+
+        # Get the color
+        color_label=first(unique(df_plot.phi))/phi_max;
+
+        # Plot
+        scatterlines!(ax_norm_plot,domain,range,
+                      color = color_label,
+                      colorrange = (color_min, color_max))
+    end
+        
+        # Plots to add systems labels
+        legend_aux_1=plot!(ax_raw_plot,[100],[100],color=:black);
+        legend_aux_2=plot!(ax_raw_plot,[100],[100],color=:black);
+
+        # Create and auxliary variable for the legend
+        legend_aux=[legend_aux_1;legend_aux_2];
+
+        # Create the labels array 
+        legend_labels=[latexstring("t=",DT*time_max,"~\\tau");latexstring("T=",labels_plot[3])]
+
+        # Create the label
+        Legend(fig[1:1,2],legend_aux,legend_labels,
+              halign=:left,
+              valign=:top
+             )
+
+        # Colobar to denote the time evolution 
+        Colorbar(fig[2:6, 2], label = L"\phi", colormap = :viridis, limits = (phi_min, phi_max))
+
+        # Crate a file name with the labels
+        file_name=string("fig_Sq_phi_series_loglog",join(string.(labels_plot)),"_time_",time_max,".png");
+
+        save(file_name, fig, px_per_unit = 300 / INCH)
+
+end
+
+
+"""
+    fig_sq_phiseries_semilog(df_combo::DataFrame, categories_figures::Vector{Symbol})
+
+Creates a figure that compares the final configuration structure factor for different packing fractions
+"""
+function fig_sq_phiseries_semilog(df_combo::DataFrame, categories_figures::Vector{Symbol})
+# Get the biggest timestep
+time_max= minimum(df_combo.timeStep);
+
+# Get all rows at the max time
+df_aux=df_combo[df_combo.timeStep .== time_max, :];
+
+# Get the domain of the packing fraction
+phi_domain=unique(df_aux.phi);
+
+# Get the data for the labels
+labels_plot=[df_aux[1, col] for col in categories_figures] # Waring: This only works for one case
+
+    # Prepare some labels
+    phi_min=minimum(phi_domain);
+    phi_max=maximum(phi_domain);
+
+    # Prepare the color code
+    color_norm  = phi_domain ./ phi_max;
+    color_min   = first(color_norm);
+    color_max   = last(color_norm);
+
+# Group by system and experiments
+df_systems=groupby(df_aux,categories_figures);
+
+    # Starts the figure
+    fig = Figure()
+    ax_raw_plot = Axis(fig[1:3, 1:1],
+                   #xlabel = L"|\vec{q}|",
+                   ylabel = L"\langle S(|\vec{q}|)\rangle",
+                   xminorticksvisible = true,
+                   xminorgridvisible = true,
+                   limits = (10^(-1.5), 10^1, nothing, nothing),
+                   xscale = log10
+                  )
+
+    for df_plot in df_systems
+        # Get the domain
+        domain=df_plot.q_mean[:];
+    
+        # Get the range
+        range=df_plot.Sq_mean[:];
+
+        # Get the color
+        color_label=first(unique(df_plot.phi))/phi_max;
+
+        # Plot
+        scatterlines!(ax_raw_plot,domain,range,
+                      color = color_label,
+                      colorrange = (color_min, color_max))
+    end
+
+    ax_norm_plot = Axis(fig[4:6, 1:1],
+                   xlabel = L"|\vec{q}|",
+                   ylabel = L"\langle S(|\vec{q}|)\rangle/S_{\max}(|\vec{q}|)",
+                   xminorticksvisible = true,
+                   xminorgridvisible = true,
+                   limits = (10^(-1.5), 10^1, nothing, nothing),
+                   xscale = log10
+                  )
+
+    for df_plot in df_systems
+        # Get the domain
+        domain=df_plot.q_mean[:];
+    
+        # Get the range
+        range=df_plot.Sq_mean_norm[:];
+
+        # Get the color
+        color_label=first(unique(df_plot.phi))/phi_max;
+
+        # Plot
+        scatterlines!(ax_norm_plot,domain,range,
+                      color = color_label,
+                      colorrange = (color_min, color_max))
+    end
+        
+        # Plots to add systems labels
+        legend_aux_1=plot!(ax_raw_plot,[100],[100],color=:black);
+        legend_aux_2=plot!(ax_raw_plot,[100],[100],color=:black);
+
+        # Create and auxliary variable for the legend
+        legend_aux=[legend_aux_1;legend_aux_2];
+
+        # Create the labels array 
+        legend_labels=[latexstring("t=",DT*time_max,"~\\tau");latexstring("T=",labels_plot[3])]
+
+        # Create the label
+        Legend(fig[1:1,2],legend_aux,legend_labels,
+              halign=:left,
+              valign=:top
+             )
+
+        # Colobar to denote the time evolution 
+        Colorbar(fig[2:6, 2], label = L"\phi", colormap = :viridis, limits = (phi_min, phi_max))
+
+        # Crate a file name with the labels
+        file_name=string("fig_Sq_phi_series_semilog",join(string.(labels_plot)),"_time_",time_max,".png");
+
+        save(file_name, fig, px_per_unit = 300 / INCH)
+
+end
+
+
+
 #=
     Start the script
 =#
@@ -371,11 +592,25 @@ categories_figures=[:phi,:chi_4,:temp,:damp,:N_heat,:N_isothermal];
 # Group by system and experiments
 df_systems=groupby(df_combo,categories_figures);
 
-#fig_sq_timeseries_loglog(df_systems,categories_figures)
+#=
+    Time series figures
 
-#fig_sq_norm_timeseries_loglog(df_systems,categories_figures)
+    fig_sq_timeseries_loglog(df_systems,categories_figures)
 
-fig_sq_timeseries_semilog(df_systems,categories_figures)
+    fig_sq_norm_timeseries_loglog(df_systems,categories_figures)
 
-fig_sq_norm_timeseries_semilog(df_systems,categories_figures)
+    fig_sq_timeseries_semilog(df_systems,categories_figures)
+
+    fig_sq_norm_timeseries_semilog(df_systems,categories_figures)
+
+=#
+
+#=
+    Compare different concentrations at the last time step
+=#
+
+fig_sq_phiseries_loglog(df_combo,categories_figures)
+
+fig_sq_phiseries_semilog(df_combo,categories_figures)
+
 
