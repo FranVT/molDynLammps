b8362370a49fe2fbf5a7bb65e861dcb0313106a1
Francisco Javier Vazquez Tavares <vazqueztfj@proton.me>
2026-07-08 16:06:00 -0600

    Analisis de simulaciones

---
+++ polymericGels/gel-batch-1/analysis/PoreAnalysis.jl
@@ -0,0 +1,298 @@
+#=
+    Script to analyze the pores 
+=#
+
+using DataFrames, CSV
+using Statistics
+
+#=
+    Functions
+=#
+"""
+    get_paths_simulation(path_dumpf_simulation::String,N_steps_Sq::Integer)
+
+Get N_steps_Sq time steps positions paths
+"""
+function get_paths_simulation(path_dumpf_simulation::String,n_steps::Integer)
+
+    # Read the directory to get the time steps stored 
+    files_traj_simulation=readdir(path_dumpf_simulation);
+    
+    # Stored timesteps
+    time_step=[parse(Int, split(s, ".")[2]) for s in files_traj_simulation]
+   
+    # Sort the time steps from low to high
+    time_step=sort(time_step);
+
+    if n_steps == 1
+        # Get the time steps to analyse last or first
+        time_step_analyze=time_step[end];
+
+        # Re-create the filenane
+        files_time_step = [replace(FILE_DUMP, "*" => string(it)) for it in time_step_analyze];
+
+        # Create the paths
+        files_traj_simulation=joinpath.(path_dumpf_simulation,files_time_step);
+
+        return files_traj_simulation
+    else
+        # Create an array with the ids of the elements to analyzed
+        ids_files_simulation=range(1,length(files_traj_simulation),length=n_steps);
+
+        # Ensure integer numbers
+        ids_files_simulation=Int64.(floor.(ids_files_simulation));
+
+        # Get the time steps to analyse
+        time_step_analyze=time_step[ids_files_simulation];
+
+        # Re-create the filenane
+        files_time_step = [replace(FILE_DUMP, "*" => string(it)) for it in time_step_analyze];
+
+        # Create the paths
+        files_traj_simulation=joinpath.(path_dumpf_simulation,files_time_step);
+
+        return files_traj_simulation
+    end
+end
+
+"""
+    get_dump(path::String)
+
+Get the data from a single dump file that stores one timeste information
+"""
+function get_dump(path::String)
+    data = split.(readlines(path), " ")[9:end]
+    HEADERS = data[1][3:end]
+    INFO = parse.(Float64, reduce(hcat, data[2:end]))'
+
+    return DataFrame(INFO, HEADERS)
+end
+
+"""
+    get_position_simulation(dump::DataFrame)
+
+Get the position of the central particles of a given dump
+"""
+function get_position_simulation(dump::DataFrame)
+    mask = (dump.type .== 1) .| (dump.type .== 2.0)
+    dump_filtered = dump[mask, :]
+
+    return [dump_filtered.x, dump_filtered.y, dump_filtered.z]
+end
+
+"""
+    wrap(dr::Real, L::Real)
+
+Compute distance tacking into account periodic boundary conditions
+"""
+function wrap(dr::Real, L::Real)
+    return dr - round(dr / L) * L
+end
+
+"""
+    probe_sphere(rad::Real, l_x::Real, l_y::Real, l_z::Real, R_CP::Real)
+
+A function that select a random point within a simulation box.
+That point is the center of a sphere which raidus increases until colides with a particle.
+Then return the radius of the sphere at colission 
+"""
+function probe_sphere(position_timestep_simulation::Vector{Vector{Float64}},l_x::Real, l_y::Real, l_z::Real, delta_rad::Real, R_CP::Real)
+    # Initialize values 
+    no_overlap=true;    # Status of no overlap 
+    rad=0.0;            # Sphere radius
+
+    # Create a random sphere center within the simulation box
+    center_rand=rand(3).*[l_x;l_y;l_z];
+
+    # Start the cycle
+    while no_overlap
+        # Increase the size of the sphere
+        rad+=delta_rad;
+
+        # Compute the distance with all particles
+        d_x=center_rand[1] .- position_timestep_simulation[1];
+        d_y=center_rand[2] .- position_timestep_simulation[2];
+        d_z=center_rand[3] .- position_timestep_simulation[3];
+
+        # Consider PBC
+        d_x=wrap.(d_x,l_x);
+        d_y=wrap.(d_y,l_y);
+        d_z=wrap.(d_z,l_z);
+
+        # Compute the magnitude of the distance
+        dist=sqrt.(sum([d_x d_y d_z].^2,dims=2));
+
+        # Create a boolean test
+        overlap_test=dist.<=(R_CP+rad);
+
+        if sum(overlap_test) != 0
+            no_overlap=false;
+            return rad
+            break
+        else
+            continue
+        end
+    end
+end
+
+"""
+    compute_pore_histogram(n_samples::Integer,l_x::Real, l_y::Real, l_z::Real, delta_rad::Real, R_CP::Real)
+
+Function that probes spheres enough times to create a histogram in order to get a pore distribution
+"""
+function compute_pore_histogram(n_samples::Integer, position_timestep_simulation::Vector{Vector{Float64}}, l_x::Real, l_y::Real, l_z::Real, delta_rad::Real, R_CP::Real)
+    
+    # Initialize the variable 
+    hist_pore=zeros(n_samples);
+
+    # Initialize auxiliary variables
+    aux_count=1;        # Counts
+    aux_ave=0.0;        # Average
+
+    for _ in 1:n_samples 
+        rad=probe_sphere(position_timestep_simulation,l_x,l_y,l_z,delta_rad,R_CP)
+
+        # Store the information at the counter
+        hist_pore[aux_count]=rad;
+
+        # Increase the counter
+        aux_count+=1;
+
+        # Update the average varaible
+        aux_ave+=rad;
+    end
+
+    # Compute the mean radius
+    mean_pore=aux_ave/aux_count;
+
+    return (hist_pore, mean_pore)
+end
+
+"""
+    save_pore_histogram(df_set::AbstractDataFrame, n_samples::Integer, n_steps::Integer, categories_system::Vector{Symbol}, categories_experiment::Vector{Symbol}, DIR_SAVE::String)
+
+This function stores the histogram of pores of a set of simulations in a time domain
+"""
+function save_pore_histogram(df_set::AbstractDataFrame, n_samples::Integer, n_steps::Integer, categories_system::Vector{Symbol}, categories_experiment::Vector{Symbol}, DIR_SAVE::String)
+
+    # Definition of parameters for the analysis
+    R_CP=0.5;                           # Radius of the central particles
+    delta_rad=0.2;                      # Increment of the radius
+    l=2*first(unique(df_set.L));        # Compute the length of the simulation box of the experiments
+    l_x = l;                            # Length of the box at x 
+    l_y = l;                            # Length of the box at y 
+    l_z = l;                            # Length of the box at z
+    n_cp=first(unique(df_set.Npart));    # Amount of central particles
+
+# Get the directories of all experiments of the system 
+    dir_set=String.(joinpath.(df_set.PARENT_DIR,df_set.dir));
+
+    # Create the paths to the files
+    path_dumpf=joinpath.(dir_set,"traj");
+
+    # Get all central particles position of all simulations at a given time domain 
+    paths_dumpf_simulations=get_paths_simulation.(path_dumpf,n_steps);
+
+    # Get the dump
+    df_dump_timestep=get_dump.(paths_dumpf_simulations);
+
+    # Get the positions
+    position_timestep_simulations=get_position_simulation.(df_dump_timestep);
+
+    # Amount of simulations
+    n_sim=length(position_timestep_simulations);
+
+    # Initialize a variable to store histograms
+    histograms_pore=[zeros(n_samples) for _ in 1:n_sim];
+
+    # Initialize a variable to mean pore 
+    means_pore=zeros(n_sim);
+
+    for (it,position_timestep_simulation) in enumerate(position_timestep_simulations)
+        # Get the histogram and mean pore of a configuration
+        (hist_pore, mean_pore)=compute_pore_histogram(n_samples,position_timestep_simulation,l_x,l_y,l_z,delta_rad,R_CP);
+
+        # Store the histogram
+        histograms_pore[it].=hist_pore;
+
+        # Store the mean
+        means_pore[it]=mean_pore;
+
+        println("One time step done")
+    end
+
+    # Reshape the array
+    histograms_pore_set=reduce(vcat,histograms_pore);
+
+    # Compute the mean of the set
+    mean_pore_set=mean(means_pore);
+
+    # Get the time step analyzed from the files
+    id_time_step=[parse(Int, match(r"traj_assembly\.(\d+)\.dumpf", s).captures[1]) for s in paths_dumpf_simulations];
+    
+    # Get the number
+    id_time_step=first(unique(id_time_step));
+
+    # Create a DataFrame to store the infomration
+    df_pore_analysis=DataFrame([repeat([id_time_step], n_samples*n_sim) repeat([mean_pore_set], n_samples*n_sim) histograms_pore_set],
+                   [:timeStep, :mean_pore_set, :histogram_pore]);
+
+   # Define a set of categories
+    categories_total=[categories_system; categories_experiment];
+
+    # Create a file name from the categories_total
+    ids_set_info=[df_set[1, col] for col in categories_total];
+
+    # Add the values of the categories to the dataframe 
+    for (col, val) in zip(categories_total, ids_set_info)
+        df_pore_analysis[!, col] .= val 
+    end
+
+    # Create a file name from the ids 
+    file_name=string("pore_analysis_",join(string.(ids_set_info)),"_step_",id_time_step,".csv");
+
+    # Save the information
+    CSV.write(joinpath(DIR_SAVE, file_name), df_pore_analysis)
+
+    # Print
+    println("Experiment ",file_name," saved\n")
+end
+
+
+#=
+    Script
+=#
+
+# Paths and directories
+DIR_MAIN = pwd();
+DAT_PATH   = joinpath(DIR_MAIN, "datFiles")
+DIR_SAVE = joinpath(DIR_MAIN,"analyzedData");
+FILE_DAT = "systemDatfiles.csv";
+FILE_FIX = "system_assembly.fixf";
+FILE_DUMP = "traj_assembly.*.dumpf";
+
+
+categories_system=[:phi,Symbol("CL-Con"),:Temperature,:damp];    # Select the categories that define a system
+categories_experiment=[:N_heat,:N_isot];  # Create categories to select different experiments (Just in case)
+
+# Read the dat file
+df_dat=CSV.read(joinpath(DAT_PATH,FILE_DAT), DataFrame);
+
+# Group by system 
+df_systems=groupby(df_dat,categories_system);
+
+# Set the parameters
+n_steps=1;                          # Amount of time steps to analyzed
+n_samples=100000;                     # Amount of sphere samples to construct the histogram
+
+# Save the mean of S(q) of a system given a set of experiments and a time domain
+for df_system in df_systems
+    # Group by experiments
+    df_experiments=groupby(df_system,categories_experiment);
+
+    # Save the mean of S(q) of an experiment given a set of simulation and a time domain 
+    foreach(df_set->save_pore_histogram(df_set,n_samples,n_steps,categories_system,categories_experiment,DIR_SAVE) ,df_experiments)
+
+    println("One system done")
+end
+
---
+++ polymericGels/gel-batch-1/analysis/PoreAnalysisFigures.jl
@@ -0,0 +1,148 @@
+#=
+    Script to create figures of pore distribution
+=#
+
+using DataFrames, CSV
+using GLMakie, LaTeXStrings 
+#=
+    Set figures 
+=#
+INCH = 96;
+PT = 4/3;
+CM = INCH / 2.54;
+
+set_theme!(
+    backgroundcolor = :white,
+    fontsize = 16PT,                     # tamaño de letra base
+    Axis = (
+        titlesize = 20PT,                 # tamaño del título del eje
+        xlabelsize = 14PT,                # tamaño etiqueta eje x
+        ylabelsize = 14PT,                 # tamaño etiqueta eje y
+        xticklabelsize = 12PT,             # tamaño números eje x
+        yticklabelsize = 12PT,              # tamaño números eje y
+        xgridstyle = :dash,               # estilo de la cuadrícula
+        ygridstyle = :dash,
+        spinewidth = 1.5PT,
+    ),
+    Legend = (
+        labelsize = 10PT,                   # tamaño texto leyenda
+        framewidth = 1.5PT,
+    ),
+    Colorbar = (
+        labelsize = 14PT,
+        ticklabelsize = 12PT,
+    ),
+    Figure = (
+        size = (15CM, 12CM)                 # tamaño de la figura (ancho, alto)
+    )
+)
+
+#=
+    Functions 
+=#
+
+#=
+    Start the script
+=#
+
+# Paths and directories
+DIR_MAIN = pwd();
+DIR_SAVE = joinpath(DIR_MAIN,"analyzed_data");
+FILE_DAT = "dat.csv";
+
+# Constants
+DT=0.001;
+
+# Read the directory 
+files=readdir(DIR_SAVE);
+
+# Get only those of the structure factor
+files=filter(s -> occursin("pore_analysis_", s), files);
+
+# Read the files
+df_files=[CSV.read(joinpath(DIR_SAVE,file), DataFrame) for file in files];
+
+# Group the Vector{DataFrame} into one DataFrame
+df_combo=reduce(vcat,df_files);
+
+# Get the time domain of the analysis
+time_domain=unique(df_combo.timeStep);
+
+# Group by the time domain 
+df_time_domain=groupby(df_combo,:timeStep);
+
+# Select one time step
+time_step=time_domain[1];
+df_time_step=df_time_domain[1];
+
+# Get the domain of the packing fraction
+phi_domain=unique(df_time_step.phi);
+
+# Categories
+categories_figures=[:phi,:chi_4,:temp,:damp,:N_heat,:N_isothermal];
+
+# Get the data for the labels
+labels_plot=[df_time_step[1, col] for col in categories_figures] # Waring: This only works for one case
+
+    # Prepare some labels
+    phi_min=minimum(phi_domain);
+    phi_max=maximum(phi_domain);
+
+    # Prepare the color code
+    color_norm  = phi_domain ./ phi_max;
+    color_min   = first(color_norm);
+    color_max   = last(color_norm);
+
+# Group by systems
+df_time_step_systems=groupby(df_time_step,categories_figures);
+
+# Set the amount of bins
+n_bins=64;
+
+# Start the figure
+    fig = Figure()
+    ax_plot = Axis(fig[1:3, 1:1],
+                   xlabel = L"r",
+                   ylabel = L"\mathrm{pdf}",
+                   xminorticksvisible = true,
+                   xminorgridvisible = true,
+                   limits = (0, nothing, 0, nothing)
+                  )
+
+        for df_system in df_time_step_systems
+            # Get the mean value
+            mean_pore=first(df_system.mean_pore_set);
+
+            # Get the color
+            color_label=first(unique(df_system.phi))/phi_max;
+
+            # Plot the histogram
+            hist!(ax_plot,df_system.histogram_pore[:], bins=n_bins, normalization = :pdf,
+                color = color_label,
+                colorrange = (color_min, color_max)
+                )
+        end 
+
+    # Colobar to denote the time evolution 
+    Colorbar(fig[2:3, 2], label = L"\phi", colormap = :viridis, limits = (phi_min, phi_max))
+
+        # Plots to add systems labels
+        legend_aux_1=plot!(ax_plot,[-1],[-1],color=:black);
+        legend_aux_2=plot!(ax_plot,[-1],[-1],color=:black);
+
+        # Create and auxliary variable for the legend
+        legend_aux=[legend_aux_1;legend_aux_2];
+
+        # Create the labels array 
+        legend_labels=[latexstring("t=",DT*time_step,"~\\tau");latexstring("T=",labels_plot[3])]
+
+        # Create the label
+        Legend(fig[1:1,2],legend_aux,legend_labels,
+              halign=:left,
+              valign=:top
+             )
+
+        # Crate a file name with the labels
+        file_name=string("fig_pore_histogram_phiseries",join(string.(labels_plot)),"_time_",time_step,".png");
+
+        save(file_name, fig, px_per_unit = 300 / INCH)
