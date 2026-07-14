90ae26c5363b461ec788ed7d82595a4c74d48262
Francisco Javier Vazquez Tavares <vazqueztfj@proton.me>
2026-07-14 12:13:03 -0600

    Tamaño de poro por lineas

---
+++ polymericGels/gel-batch-1/analysis/PoreAnalysis_lines.jl
@@ -0,0 +1,220 @@
+# =========================================================================
+# Program:      PoreAnalysis_lines 
+# Description:  Calculate the distribution of pore lengths in a system
+#               of spheres with a radius of 0.5, launching segments between pairs of
+#               particles chosen at random and verifying that they do not intersect
+#               any other particle.
+#               Reads multiple dump files (LAMMPS dump) and writes a
+#               histogram for each configuration.
+# =========================================================================
+
+using DataFrames, CSV
+using Statistics, Random
+
+#=
+    Functions
+=#
+"""
+    get_paths_simulation(path_dumpf_simulation::String,N_steps_Sq::Integer)
+
+Get N_steps_Sq time steps positions paths
+"""
+function get_paths_simulation(path_dumpf_simulation::String,n_steps::Integer)
+
+    # Read the directory to get the time steps stored 
+    files_traj_simulation=readdir(path_dumpf_simulation);
+    
+    # Stored timesteps
+    time_step=[parse(Int, split(s, ".")[2]) for s in files_traj_simulation]
+   
+    # Sort the time steps from low to high
+    time_step=sort(time_step);
+
+    if n_steps == 1
+        # Get the time steps to analyse last or first
+        time_step_analyze=time_step[end];
+
+        # Re-create the filenane
+        files_time_step = [replace(FILE_DUMP, "*" => string(it)) for it in time_step_analyze];
+
+        # Create the paths
+        files_traj_simulation=joinpath.(path_dumpf_simulation,files_time_step);
+
+        return files_traj_simulation
+    else
+        # Create an array with the ids of the elements to analyzed
+        ids_files_simulation=range(1,length(files_traj_simulation),length=n_steps);
+
+        # Ensure integer numbers
+        ids_files_simulation=Int64.(floor.(ids_files_simulation));
+
+        # Get the time steps to analyse
+        time_step_analyze=time_step[ids_files_simulation];
+
+        # Re-create the filenane
+        files_time_step = [replace(FILE_DUMP, "*" => string(it)) for it in time_step_analyze];
+
+        # Create the paths
+        files_traj_simulation=joinpath.(path_dumpf_simulation,files_time_step);
+
+        return files_traj_simulation
+    end
+end
+
+"""
+    get_dump(path::String)
+
+Get the data from a single dump file that stores one timeste information
+"""
+function get_dump(path::String)
+    data = split.(readlines(path), " ")[9:end]
+    HEADERS = data[1][3:end]
+    INFO = parse.(Float64, reduce(hcat, data[2:end]))'
+
+    return DataFrame(INFO, HEADERS)
+end
+
+"""
+    get_position_simulation(dump::DataFrame)
+
+Get the position of the central particles of a given dump
+"""
+function get_position_simulation(dump::DataFrame)
+    mask = (dump.type .== 1) .| (dump.type .== 2.0)
+    dump_filtered = dump[mask, :]
+
+    return reduce(hcat,[dump_filtered.x, dump_filtered.y, dump_filtered.z])
+end
+
+"""
+    wrap(dr::Real, L::Real)
+
+Compute distance tacking into account periodic boundary conditions
+"""
+function wrap(dr::Real, L::Real)
+    return dr - round(dr / L) * L
+end
+
+
+
+#=
+    Script
+=#
+
+# Set sed
+Random.seed!(1234)
+
+# Paths and directories
+DIR_MAIN = pwd();
+DAT_PATH   = joinpath(DIR_MAIN, "datFiles")
+DIR_SAVE = joinpath(DIR_MAIN,"analyzedData");
+FILE_DAT = "systemDatfiles.csv";
+FILE_FIX = "system_assembly.fixf";
+FILE_DUMP = "traj_assembly.*.dumpf";
+
+
+categories_system=[:phi,Symbol("CL-Con"),:Temperature,:damp];    # Select the categories that define a system
+categories_experiment=[:N_heat,:N_isot];  # Create categories to select different experiments (Just in case)
+
+# Read the dat file
+df_dat=CSV.read(joinpath(DAT_PATH,FILE_DAT), DataFrame);
+
+# Group by system 
+df_systems=groupby(df_dat,categories_system);
+
+# Set the parameters
+n_steps=1;                          # Amount of time steps to analyzed
+n_samples=10000;                    # Amount of tries per simulation
+
+# Select one system: same categories of system
+df_system=df_systems[1]
+    
+# Group by experiments: same categories of experiment
+df_experiments=groupby(df_system,categories_experiment);
+
+# Select one experiment: Number of simulations
+df_set=df_experiments[1]
+
+
+    # Definition of parameters for the analysis
+    R_CP=0.5;                           # Radius of the central particles
+    l_half=first(unique(df_set.L));        # Compute the length of the simulation box of the experiments
+    l_half_x = l_half;
+    l_half_y = l_half;
+    l_half_z = l_half;
+    l_x = 2*l_half_x;                            # Length of the box at x 
+    l_y = 2*l_half_y;                            # Length of the box at y 
+    l_z = 2*l_half_z;                            # Length of the box at z
+    n_cp=Int64(first(unique(df_set.Npart)));    # Amount of central particles
+    bin_size=0.2;       # Size of the bins. Is equal to the patches radius
+
+    # Compute the amount of bins
+    n_bins = trunc(Int64,sqrt(4*l_x^2 + l_y^2 + l_z^2)/bin_size) + 1;
+
+# Get the directories of all experiments of the system 
+    dir_set=String.(joinpath.(df_set.PARENT_DIR,df_set.dir));
+
+    # Create the paths to the files
+    path_dumpf=joinpath.(dir_set,"traj");
+
+    # Get all central particles position of all simulations at a given time domain 
+    paths_dumpf_simulations=get_paths_simulation.(path_dumpf,n_steps);
+
+    # Get the dump
+    df_dump_timestep=get_dump.(paths_dumpf_simulations);
+
+    # Get the positions
+    position_timestep_simulations=get_position_simulation.(df_dump_timestep);
+
+    # Amount of simulations
+    n_sim=length(position_timestep_simulations);
+
+    # Allcoate memory
+    hist_pore_length = zeros(n_bins);    # Histogram of the lengths of the pores
+    
+    # Select one set of positions
+    position_simulation=position_timestep_simulations[1];
+
+    # Start counter of crashes
+    choles=0;
+
+    
+    #while choles <= n_samples
+        # Select one random particle
+        id_i = rand(1:n_cp);
+
+        # Select another random particle
+        id_j = id_i;
+        while id_j == id_i
+            global id_j = rand(1:n_cp);
+        end
+
+        # Compute the distance between particles with periodic boundary conditions
+        v_ij = wrap.(position_simulation[id_j,:] .-  position_simulation[id_i,:], [l_x, l_y, l_z]);
+
+        # Create two unit vectors
+        v_1=rand(-1:0.001:1,3);
+        u_1=v_1/sqrt(v_1'*v_1);
+
+        v_2=rand(-1:0.001:1,3);
+        u_2=v_2/sqrt(v_2'*v_2);
+
+        # Create the direction of the line
+        v_line=(0.5)*(u_2 .- u_1);
+
+        # Add the direction to the distances of particles
+        v_chole = v_ij .+ v_line;
+
+        # Compute unit vector
+        d_chole = sqrt(v_chole'*v_chole);
+        u_chole = v_chole/d_chole;
+
+        id_bin = trunc(Int64,d_chole/bin_size);
+
+        if id_bin > n_bins 
+            continue # Ignore if it is outside the domain
+        end
+
+    #end
+
+
