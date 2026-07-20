751fa6449c982aed94ed3bbe702b9759b289d145
Francisco Javier Vazquez Tavares <vazqueztfj@proton.me>
2026-07-20 08:37:05 -0600

    Ver que onda con el tamaño de poro

--- polymericGels/gel-batch-1/analysis/pore_length_claudia/fig.jl
+++ polymericGels/gel-batch-1/analysis/pore_length_claudia/fig.jl
@@ -2,12 +2,15 @@
 using GLMakie
 using CSV, DataFrames
 
+
 # Paths and directories
 DIR_MAIN = pwd();
 
+
+
 # filename
-filename_initial =  "pore_length_histogram_0.050.050.051.0500000.09.5e6_step_0_simulation_1.csv";
-filename_final =    "pore_length_histogram_0.050.050.051.0500000.09.5e6_step_10000000_simulation_1.csv";
+filename_initial =  "pl_0.csv";
+filename_final =    "pl_10000000.csv";
 
 # Get the data
 df_initial=CSV.read(joinpath(DIR_MAIN,filename_initial), DataFrame);
@@ -16,6 +19,5 @@ df_final=CSV.read(joinpath(DIR_MAIN,filename_final), DataFrame);
 # Create categroies for the labels
 categories_system=[:phi,Symbol("CL-Con"),:Temperature,:damp];    # Select the categories that define a system
 categories_experiment=[:N_heat,:N_isot];  # Create categories to select different experiments (Just in case)
-
 
 
